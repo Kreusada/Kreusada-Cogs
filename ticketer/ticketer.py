4cb7e22a2855b73932c7b38a01e3ca9bb4f52f8c
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-18 20:54:20 +0100

    revert

--- ticketer/ticketer.py
+++ ticketer/ticketer.py
@@ -179,7 +179,7 @@ class Ticketer(commands.Cog):
         ctx,
         author,
         *,
-        reason: Optional[str] = f"No reason provided. Submitted by {ctx.message.author.mention}.",
+        reason: Optional[str] = "No reason provided. Submitted by {ctx.message.author.mention}.",
     ):
         """Create a ticket."""
         if await self._check_settings(ctx):
