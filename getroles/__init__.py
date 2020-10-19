f9cf240101cf674307c59f4ec1bd38300b2f8a57
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-19 18:03:57 +0100

    Create __init__.py

---
+++ getroles/__init__.py
@@ -0,0 +1,12 @@
+from .getroles import Getroles
+from pathlib import Path
+import json
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+async def setup(bot):
+    cog = Getroles()
+    await cog.register_casetypes()
+    bot.add_cog(cog)
