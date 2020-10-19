f1b0590d7da6f813eb9049300e808006de71bda9
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-19 22:52:15 +0100

    Removed `Random:` from randomurls

--- getroles/getroles.py
+++ getroles/getroles.py
@@ -46,7 +46,7 @@ class Getroles(commands.Cog):
     async def randomurl(self, ctx):
         """Testing random responses with URLs."""
         urlList = ["https://cdn.discordapp.com/attachments/745608075670585344/767866788935368744/Storm_Pyramid_Z.png,https://cdn.discordapp.com/attachments/745608075670585344/767866847390728222/Silver_Aegon.png"]
-        await ctx.send("Random: {0}".format(random.choice(urlList)))
+        await ctx.send("{0}".format(random.choice(urlList)))
 
 #    @getroles.command()
 #    async def testrole(
