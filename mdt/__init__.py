28110a4fa5d117c11eb3d6c261ac08efd7a8a7e5
Kreusada <kreusada@github.com>
2020-10-19 12:44:35 +0100

    del mdt

--- mdt/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mdt import mdt
-
-def setup(bot):
-    bot.add_cog(mdt())
--- mdt/mdt.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-
-class mdt(commands.Cog):
-    """MDT Invite link"""
-
-    @commands.command()
-    async def gminfo(self, ctx):
-        """MDT Invite link"""
-        await ctx.send("https://discord.gg/JmCFyq7")
