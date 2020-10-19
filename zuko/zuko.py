7ff84f68acc3e803c71d07cd2452873dc55f242c
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-19 17:41:37 +0100

    Update zuko.py

--- zuko/zuko.py
+++ zuko/zuko.py
@@ -4,6 +4,14 @@ from redbot.core import commands, Config
 class Zuko(commands.Cog):
   """Zuko"""
 
+def __init__(self, bot):
+  self.bot = bot
+  self.config = Config.get_conf(
+    self,
+    identifier=
+    force_registration=True,
+  )
+
 #@commands.group()
 #async def zuko(self, ctx):
 #  """All Zuko settings."""
