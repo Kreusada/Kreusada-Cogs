0b7e4aa8b2bd04cd795500b0d977570e95a50fe2
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-19 15:37:46 +0100

    Update __init__.py

--- zuko/__init__.py
+++ zuko/__init__.py
@@ -1,8 +1,5 @@
 from .zuko import Zuko
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 async def setup(bot):
     cog = Zuko()
     bot.add_cog(cog)
