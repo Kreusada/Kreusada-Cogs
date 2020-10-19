a9d926f804e3fba0c7a7ab9f1b296b2c34dd6103
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-19 15:13:47 +0100

    Update zuko.py

--- zuko/zuko.py
+++ zuko/zuko.py
@@ -1,1 +1,33 @@
-from redbot.core import commands
+from redbot.core import commands, checks, Config
+import discord
+import json
+import os
+import discord
+from discord.utils import get
+
+class Zuko(commands.Cog):
+  """Zuko"""
+
+@commands.group()
+async def zuko(self, ctx):
+  """All Zuko settings."""
+  pass
+
+@zuko.command()
+async def zukosimple(self, ctx):
+  """Simple test response from Zuko."""
+  await ctx.send('**You got the `coding` just right. :white_check_mark:.**')
+
+@commands.group()
+async def zukosubcats(self, ctx):
+  """Testing sub-categories."""
+  pass
+
+@zukosubcats.command():
+  async def subcat(self, ctx):
+    """Here should be your sub category. *Should be*."""
+    await ctx.send('Subcategory command **successful**. :sparkles:')
+
+
+  
+
