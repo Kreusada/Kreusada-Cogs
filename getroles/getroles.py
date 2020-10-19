bc2aa5ed336f4c8896b460337a885994ed860d3e
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-19 19:16:39 +0100

    Removed descriptions from test3 and test4

--- getroles/getroles.py
+++ getroles/getroles.py
@@ -27,12 +27,10 @@ class Getroles(commands.Cog):
         
     @test2.group()
     async def test3(self, ctx):
-        """Response from Test 3."""
         await ctx.send(f"Test 3 responded.")
         
     @test2.group()
     async def test4(self, ctx):
-        """Response from Test 4."""
         await ctx.send(f"Test 4 responded.")
 
 #    @getroles.command()
