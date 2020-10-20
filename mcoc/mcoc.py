0133118194281cd2da03f7ef572909a0c345d88e
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-20 21:00:36 +0100

    Update mcoc.py

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -28,11 +28,5 @@ class Mcoc(commands.Cog):
   @champ.group()
   async def featured(self, ctx):
     """Find champion featured images."""
-    data = Embed.create(self, ctx, title='{0} Featured Image')
-    image = (random.choice(FEATUREDS))
-    name = ctx.author.name
-    data.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-    data.set_image(url=image)
-    await ctx.send(embed=data)
     
       
