ea1e1adb4d2bb1e94afdb9b10397d59d769cecc7
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-20 23:56:07 +0100

    Capitalized... maybe

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -30,7 +30,7 @@ class Mcoc(commands.Cog):
     async def featured(self, ctx, champion: str):
         try:
             data = Embed.create(self, ctx, title="{0}'s Featured Image.".format(
-                champion), image=FEATUREDS[champion.lower()])
+                champion.capitalize()), image=FEATUREDS[champion.lower()])
             await ctx.send(embed=data)
         except KeyError:
             await ctx.send("`I could not find that champion. Either the champion does not exist or the key was entered incorrectly.`")
