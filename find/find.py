5359d8da1a10c30076da027e30624367e1b2a780
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-20 15:37:49 +0100

    Silly indent

--- find/find.py
+++ find/find.py
@@ -205,7 +205,7 @@ class Find(commands.Cog):
     """Stone 3 Badge."""
     await ctx.send(f"https://media.discordapp.net/attachments/401476363707744257/738084098857238670/EA938C0B0C2AE3E6DB91514F5F8768C4F033D373.png")
     
- @participation.group(name="1", invoke_without_command=True, pass_context=True)
+  @participation.group(name="1", invoke_without_command=True, pass_context=True)
   async def twentyone(self, ctx):
     """Participation Badge."""
     author = ctx.message.author
