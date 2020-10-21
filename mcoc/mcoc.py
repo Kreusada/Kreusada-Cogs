a85dac62750ebaae617d50ca61e73aa6a25c2528
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-21 22:27:00 +0100

    Major fixes required [burndown]

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -23,11 +23,12 @@ class Mcoc(commands.Cog):
     @commands.group(invoke_without_command=True)
     async def crystal(self, ctx):
         """Chooses a random champion from MCOC."""
+        await ctx.send("`Unfortunately, this command is currently down for maintenance.\n`Unexpected expenses may arrise for Kelldor, as Demaratus was using his auntm.ai URLs.`\n\nFor more updates, please stay tuned to my support server: https://discord.gg/JmCFyq7")
         # author = ctx.message.author
-        data = Embed.create(self, ctx, title='You got... :gem:')
-        image = (random.choice(CRYSTAL))
-        data.set_image(url=image)
-        await ctx.send(embed=data)
+#        data = Embed.create(self, ctx, title='You got... :gem:')
+#        image = (random.choice(CRYSTAL))
+#        data.set_image(url=image)
+#        await ctx.send(embed=data)
 
     @commands.group()
     async def champ(self, ctx):
@@ -35,13 +36,14 @@ class Mcoc(commands.Cog):
 
     @champ.command()
     async def featured(self, ctx, alias: str):
-        champion = FULL_NAMES.get(alias, alias)
-        try:
-            data = Embed.create(self, ctx, title="Featured Image.".format(
-                champion.capitalize()), image=FEATUREDS[champion.lower()])
-            await ctx.send(embed=data)
-        except KeyError:
-            await ctx.send("`I could not find that champion.`\n`Please check your spelling, or your alias might not be available just yet.` :fearful:\n\n`Additionally, this command has not yet been completed. I'm working on it asap!`")
+        await ctx.send("`Unfortunately, this command is currently down for maintenance.\n`Unexpected expenses may arrise for Kelldor, as Demaratus was using his auntm.ai URLs.`\n\nFor more updates, please stay tuned to my support server: https://discord.gg/JmCFyq7")
+#        champion = FULL_NAMES.get(alias, alias)
+#        try:
+#            data = Embed.create(self, ctx, title="Featured Image.".format(
+#                champion.capitalize()), image=FEATUREDS[champion.lower()])
+#            await ctx.send(embed=data)
+#        except KeyError:
+#            await ctx.send("`I could not find that champion.`\n`Please check your spelling, or your alias might not be available just yet.` :fearful:\n\n`Additionally, this command has not yet been completed. I'm working on it asap!`")
 
     @champ.command()
     async def portrait(self, ctx, champion: str):
