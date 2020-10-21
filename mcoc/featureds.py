84f5b2e7377f2552ed6d5239927d1afd4f93e8a8
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-21 09:56:18 +0100

    Testing for aliases

--- mcoc/featureds.py
+++ mcoc/featureds.py
@@ -1,5 +1,5 @@
 FEATUREDS = {
-    "abom": "https://auntm.ai/resources/ui/uigacha/featured/gachachaseprize_256x256_abomination.png",
+    "abom": {"url":"https://auntm.ai/resources/ui/uigacha/featured/gachachaseprize_256x256_abomination.png", "aliases": ["abom1", "abom2", "abom3"]},
     "aegon": "https://auntm.ai/resources/ui/uigacha/featured/gachachaseprize_256x256_aegon.png",
     "agentvenom": "https://auntm.ai/resources/ui/uigacha/featured/gachachaseprize_256x256_agent_venom.png",
     "airwalker": "https://auntm.ai/resources/ui/uigacha/featured/gachachaseprize_256x256_airwalker.png",
