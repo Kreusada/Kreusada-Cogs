ec11ff3d3fe72b0a286012675204ae7fbbc1ec20
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-21 10:45:42 +0100

    Crystals have gone, for now

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -12,16 +12,16 @@ from discord.utils import get
 class Mcoc(commands.Cog):
     """Mcoc"""
 
-    @commands.group(invoke_without_command=True)
-    async def crystal(self, ctx):
-        """Chooses a random champion from MCOC."""
-        # author = ctx.message.author
-        data = Embed.create(self, ctx, title='You got... :gem:')
-        image = (random.choice(FEATUREDS))
-        # name = ctx.author.name
-        # data.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        data.set_image(url=image)
-        await ctx.send(embed=data)
+#    @commands.group(invoke_without_command=True)
+#    async def crystal(self, ctx):
+#        """Chooses a random champion from MCOC."""
+#        # author = ctx.message.author
+#        data = Embed.create(self, ctx, title='You got... :gem:')
+#        image = (random.choice(FEATUREDS))
+#        # name = ctx.author.name
+#        # data.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+#        data.set_image(url=image)
+#        await ctx.send(embed=data)
 
     @commands.group()
     async def champ(self, ctx):
