37e5cff207989afae0fc3b348b009d6c81493ea9
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-23 20:34:07 +0100

    Corrected indents

--- find/find.py
+++ find/find.py
@@ -315,21 +315,20 @@ class Find(commands.Cog):
     data.set_image(url=image)
     await ctx.send(embed=data)
     
-    @find.command()
-    async def edition(self, ctx, *, name: str = None): 
-      """Testing nickname change"""
-      if name is None: 
-        await ctx.author.edit(nick=ctx.author.name) 
-        return await ctx.send("Your nickname was reset.")
+  @find.command()
+  async def edition(self, ctx, *, name: str = None): 
+    """Become an edition of Kreusada."""
+    if name is None: 
+      await ctx.author.edit(nick=ctx.author.name) 
+      return await ctx.send("Your nickname was reset.")
     user = ctx.author
     before = ctx.author.name
     after = name
     tag = "Kreusada - {0} Edition".format(after)
     try:
       await user.edit(nick=tag)
-    except discord.Forbidden:
+      except discord.Forbidden:
       await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
-      
       await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
     
 #  @find.group()
