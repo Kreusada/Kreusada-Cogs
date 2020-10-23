2831d1035be75b47b937854520327b2958738e41
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-23 21:29:54 +0100

    Indentation

--- find/find.py
+++ find/find.py
@@ -320,17 +320,17 @@ class Find(commands.Cog):
     """Become an edition of Kreusada."""
     if name is not None: 
       await ctx.author.edit(nick=ctx.author.name) 
-    return await ctx.send("Your nickname was reset.")
-    user = ctx.author
-    before = ctx.author.name
-    after = name
-    tag = "Kreusada - {0} Edition".format(after)
-    try:
-      await user.edit(nick=tag)
-    except discord.Forbidden:
-      await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
+      return await ctx.send("Your nickname was reset.")
+      user = ctx.author
+      before = ctx.author.name
+      after = name
+      tag = "Kreusada - {0} Edition".format(after)
+      try:
+        await user.edit(nick=tag)
+      except discord.Forbidden:
+        await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
       
-      await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
+        await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
     
 #  @find.group()
 #  async def donate(self, ctx):
