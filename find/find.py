be69eed266a6e111666472b007a618a27016af04
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-23 20:35:49 +0100

    fixed

--- find/find.py
+++ find/find.py
@@ -328,9 +328,9 @@ class Find(commands.Cog):
     try:
       await user.edit(nick=tag)
       except discord.Forbidden:
-      await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
+        await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
       
-      await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
+        await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
     
 #  @find.group()
 #  async def donate(self, ctx):
