cfc5bedecf606db31bee406440ed48ab0c427e40
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-23 21:37:24 +0100

    indents indents indents...

--- find/find.py
+++ find/find.py
@@ -321,16 +321,16 @@ class Find(commands.Cog):
     if name is None: 
       await ctx.author.edit(nick=ctx.author.name) 
       return await ctx.send("Your nickname was reset.")
-      user = ctx.author
-      before = ctx.author.name
-      after = name
-      tag = "Kreusada - {0} Edition".format(after)
-      try:
-        await user.edit(nick=tag)
-      except discord.Forbidden:
-        await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
+    user = ctx.author
+    before = ctx.author.name
+    after = name
+    tag = "Kreusada - {0} Edition".format(after)
+    try:
+      await user.edit(nick=tag)
+    except discord.Forbidden:
+      await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
       
-        await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
+    await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
     
 #  @find.group()
 #  async def donate(self, ctx):
