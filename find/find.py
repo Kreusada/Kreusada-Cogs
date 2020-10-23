b21efdd87f87e2bddf4860b623d432018ddd564f
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-23 20:41:59 +0100

    test

--- find/find.py
+++ find/find.py
@@ -318,7 +318,7 @@ class Find(commands.Cog):
   @find.command()
   async def edition(self, ctx, *, name: str = None): 
     """Become an edition of Kreusada."""
-    if name is None: 
+    if name is  not None: 
       await ctx.author.edit(nick=ctx.author.name) 
     return await ctx.send("Your nickname was reset.")
     user = ctx.author
@@ -330,7 +330,7 @@ class Find(commands.Cog):
     except discord.Forbidden:
       await ctx.send("Your nickname could not be changed, I don't have permissions or you are higher than me in the role heirarchy.")
       
-        await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
+      await ctx.send("You are now an Edition of Kreusada. Your nickname was successfully changed to: ``{}``".format(tag))
     
 #  @find.group()
 #  async def donate(self, ctx):
