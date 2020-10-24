3b051195af6e4f5bd30550211d73b6dfad4d1ed6
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-24 19:20:20 +0100

    testing

--- random/random.py
+++ random/random.py
@@ -2,3 +2,19 @@ from redbot.core import commands, checks, Config
 from mdtembed import Embed
 import discord
 import random
+
+class random(commands.Cog):
+  """Random choice for multiple subjects."""
+  def __init__(self, bot):
+    self.bot = bot
+    self.config = Config.get_conf(
+      self, identifier=153607829, force_registration=True)
+    
+    @commands.group(invoke_without_command=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def futurehousemusic(self, ctx):
+      """Chooses a random song from FHM."""
+      data = Embed.create(self, ctx, title='You got... :gem:')
+      description = (random.choice(FHM))
+      data.set_description(url=description)
+      await ctx.send(embed=data)
