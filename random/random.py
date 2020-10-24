1a1fa4e7f3da64212d98596e2b154cefbb5e8e86
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-24 20:07:41 +0100

    testing

--- random/random.py
+++ random/random.py
@@ -12,11 +12,11 @@ class Random(commands.Cog):
     self.config = Config.get_conf(
       self, identifier=153607829, force_registration=True)
     
-    @commands.group(invoke_without_command=True) 
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def futurehousemusic(self, ctx):
-      """Chooses a random song from FHM."""
-      data = Embed.create(self, ctx, title='You got... :gem:')
-      description = (random.choice(FHM))
-      data.set_description(url=description)
-      await ctx.send(embed=data)
+  @commands.group(invoke_without_command=True) 
+  @commands.cooldown(1, 10, commands.BucketType.user)
+  async def futurehousemusic(self, ctx):
+    """Chooses a random song from FHM."""
+    data = Embed.create(self, ctx, title='You got... :gem:')
+    description = (random.choice(FHM))
+    data.set_description(url=description)
+    await ctx.send(embed=data)
