229ea90bdc0c0fea6e73c8be94908c3c480db005
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-25 23:32:38 +0000

    Testing

--- dadjokes/dadjokes.py
+++ dadjokes/dadjokes.py
@@ -27,7 +27,7 @@ class DadJokes(commands.Cog):
         joke = await self.get_joke()
         data = Embed.create(self, ctx, title='Demaratus Dad Jokes :joy:',
                             description=joke)
-        image = (f"https://media.discordapp.net/attachments/758775890954944572/768582555116109854/demaratusfull.png?width=957&height=677")
+        image = (f"https://pbs.twimg.com/profile_banners/1206854079739244545/1603663401/1500x500")
         data.set_author
         data.set_image(url=image)
         await ctx.send(embed=data)
