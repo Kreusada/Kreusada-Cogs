4f87e35abd8fb2bd38b5b77a2b16e7cff4e4d652
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-26 21:22:30 +0000

    Update bc.py

--- mcoc/bc.py
+++ mcoc/bc.py
@@ -1,5 +1,6 @@
 BCB = {
   "45 Units": "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/b/b0/Units_icon.png/revision/latest/scale-to-width-down/340?cb=20190505220131",
+  "15 Units": "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/b/b0/Units_icon.png/revision/latest/scale-to-width-down/340?cb=20190505220131",
   "10,000 Gold": "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/2/2b/Gold_icon.png/revision/latest?cb=20190505220317",
   "2,000 Gold": "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/2/2b/Gold_icon.png/revision/latest?cb=20190505220317",
   "5,000 Gold": "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/2/2b/Gold_icon.png/revision/latest?cb=20190505220317",
