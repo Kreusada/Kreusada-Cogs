cb5041ff8bf2d668f454884e032377d81b3ee5d7
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-30 18:00:46 +0000

    Testing

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -29,6 +29,7 @@ class Mcoc(commands.Cog):
         data = Embed.create(self, ctx, title='You got... :gem:')
         image = (random.choice(CRYSTAL))
         description = "Testing"
+        data.description = description
         data.set_image(url=image)
         await ctx.send(embed=data)
 
