0dffde5f12f39845db8ebcee2f0ce5eb9149c87d
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-10-31 20:03:56 +0000

    Added UCL

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -80,11 +80,41 @@ class Mcoc(commands.Cog):
     @battlechip.group(invoke_without_command=True)
     async def uncollected(self, ctx):
         """Open an uncollected battlechip crystal."""
-        author = ctx.message.author
-        data = Embed.create(self, ctx, title='Oopsies!')
-        image = (f"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Under_construction_icon-yellow.svg/1200px-Under_construction_icon-yellow.svg.png")
-        description = "This command is still being developed. It should be finished by this week! :heart:"
-        data.description = description
-        data.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        data.set_image(url=image)
+        data = Embed.create(self, ctx)
+        drop_rate = random.randint(1, 100)
+        if drop_rate < 0.02:
+            link = BCB[0]
+            title = "5 Star Punisher"
+            description = "This tier has a `0.02%` chance.\nCongratulations!\nMessage Kreusada#0518 with a screenshot to be added to the hall of fame!"
+        elif drop_rate < 0.65:
+            link = BCB[0]
+            title "4 Star Punisher"
+            description = "This tier has a `0.65%` chance.\nCongratulations!"
+        elif drop_rate < 0.35:
+            link = BCB[1]
+            title = "Five Energy Refills"
+            description = "This tier has a `0.35%` chance.\nCongratulations!"
+        elif drop_rate < 2:
+            link = BCB[2]
+            title = "225 Units"
+            description = ""
+        elif drop_rate < 6:
+            link = BCB[2]
+            title = "75 Units"
+            description = ""
+        elif drop_rate < 30:
+            link = BCB[3]
+            title = "50,000 Gold"
+            description = ""
+        elif drop_rate < 45
+            link = BCB[3]
+            title = "25,000 Gold"
+            description = ""
+        else:
+            link = BCB[3]
+            title = "10,000 Gold"
+            description = ""
+        data.title = "You got {}!".format(title)
+        data.description = "{}".format(description)
+        data.set_image(url=link)
         await ctx.send(embed=data)
