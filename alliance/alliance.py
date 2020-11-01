26c7d579b8254a42ccbf4e3766b0ab7eacb16fd1
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-01 09:36:59 +0000

    Improved UI

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -6,14 +6,14 @@ class Alliance(commands.Cog):
     """Tools for your alliance on MCOC."""
 
     @commands.command()
-    async def timezone(self, ctx, name):
+    async def timezone(self, ctx, timezone):
         if name is None:
             await ctx.author.edit(nick=ctx.author.name)
             return await ctx.send("Your timezone was reset. It will no longer show on your Discord nickname.")
         user = ctx.author
         before = ctx.author.name
         after = name
-        tag = "{0} - [{1}]".format(before, after)
+        tag = "{0} [{1}]".format(before, after)
         try:
             await user.edit(nick=tag)
         except discord.Forbidden:
