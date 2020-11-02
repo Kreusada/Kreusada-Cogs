79ad3e4b2866929f763dfe726e8619b83c4f7fec
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-02 16:35:37 +0000

    InDeNtAtIoN eRrOr

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -353,7 +353,7 @@ class Mcoc(commands.Cog):
     
     @participation.group(name="1", invoke_without_command=True, pass_context=True)
     async def twentyone(self, ctx):
-    """Participation Badge."""
+        """Participation Badge."""
         author = ctx.message.author
         data = Embed.create(self, ctx, title='Participation Badge :trophy:')
         image = (f"https://media.discordapp.net/attachments/401476363707744257/738083790886535228/DA7D39277836A9CF1B39A68D37EAF99999B366C7.png")
