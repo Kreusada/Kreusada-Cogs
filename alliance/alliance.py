27d170688614525f18a3b3b427ca9dedbaa4fd00
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-03 19:43:04 +0000

    Author probably won't be required for its context

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -33,10 +33,10 @@ class Alliance(commands.Cog):
     @alliancealert.command()
     async def aqstart(self, ctx, invoke_without_command=True, pass_context=True):
         """Alliance Quest has started!"""
-        author = ctx.message.author
+#        author = ctx.message.author
         data = Embed.create(self, ctx, title='Alliance Quest has STARTED!')
         image = ("https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480")
-        name = ctx.author.name
-        data.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+#        name = ctx.author.name
+#        data.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         data.set_image(url=image)
         await ctx.send(embed=data)
