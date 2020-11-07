69393eca2e6de48d7f003e098a6fdf7dac34828c
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-07 16:18:39 +0000

    Placeholding for now

--- demevents/demevents.py
+++ demevents/demevents.py
@@ -7,3 +7,11 @@ from .deembed import Embed
 from discord.utils import get
 
 class Demevents(commands.Cog):
+  """Events specifically for Demaratus."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = Config.get_conf(
+            self, identifier=153607829, force_registration=True)
+        self.config.register_guild()
+        
