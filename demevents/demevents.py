3809a1cc7de806d77952a8f82e3e1c5382856643
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-07 16:16:23 +0000

    Update demevents.py

---
+++ demevents/demevents.py
@@ -0,0 +1,9 @@
+from redbot.core import commands, checks, Config
+import discord
+import random
+import json
+import os
+from .deembed import Embed
+from discord.utils import get
+
+class Demevents(commands.Cog):
