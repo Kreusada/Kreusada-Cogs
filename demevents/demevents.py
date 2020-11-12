a4ad5791a0170e71a162c9e3607957606244e09a
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-12 16:40:24 +0000

    Testing

--- demevents/demevents.py
+++ demevents/demevents.py
@@ -6,12 +6,25 @@ import os
 from .deembed import Embed
 from discord.utils import get
 
-class Demevents(commands.Cog):
-  """Events specifically for Demaratus."""
+class kreustools(commands.Cog):
+  """Tools from Kreusada and Others."""
 
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(
             self, identifier=153607829, force_registration=True)
         self.config.register_guild()
+        
+      @commands.command(pass_context=True, no_pm=True)
+    async def showtopic(self, ctx, channel: discord.TextChannel = None):
+        """Show the Channel Topic in a chat channel."""
+        if channel is None:
+            channel = ctx.message.channel
+        topic = channel.topic
+        if topic is not None and topic != '':
+            data = self.Embed.create(ctx, title='#{} Topic :star:'.format(
+                                     channel.name),
+                                     description=topic)
+            data.set_thumbnail(url=ctx.message.guild.icon_url)
+            await ctx.send(embed=data)
         
