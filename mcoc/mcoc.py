fb96d4627a277e05be4756f1a8081aad5b3d99b9
KREUSADA <67752638+KREUSADA@users.noreply.github.com>
2020-11-13 18:45:48 +0000

    Testing for extra field

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -238,13 +238,13 @@ class Mcoc(commands.Cog):
                 description="Please choose one of the tiers below :arrow_down:"
             )
             normal = "\n".join([t.capitalize() for t in tiers.keys()])
-#            embed.add_field(
+            embed.add_field(
                 # Unfortunatly I have to do this to make sure that participation gets in the list :/
-#                name="Badges", value="{}\nParticipation".format(normal)
-#            )
+                name="Badges", value="{}\nParticipation".format(normal)
+            )
 
             normal = "\n".join(tiers)
-            embed.add_field(name="Badges", value="{}\nparticipation".format(normal)) # Unfortunatly I have to do this to make sure that participation gets in the list :/
+#            embed.add_field(name="Badges", value="{}\nparticipation".format(normal)) # Unfortunatly I have to do this to make sure that participation gets in the list :/
             return await ctx.send(embed=embed)
         if tier == "participation":
             embed = Embed.create(
