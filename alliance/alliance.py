1904b98497daddda78b6e42950c61ac3c0a787a7
kreusjackel <67752638+kreusjackel@users.noreply.github.com>
2020-11-14 16:16:26 +0000

    Hopefully this will work!

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -52,11 +52,11 @@ class Alliance(commands.Cog):
         
         
     @alliancealert.command(invoke_without_command=True, pass_context=True, aliases=["aa", "alert"])
-    async def aqstart(self, ctx):
+    async def aqstart(self, ctx, role: discord.Role):
         """Alliance Quest has started!"""
         data = Embed.create(self, ctx, title='Alliance Quest has STARTED!')
         image = ("https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480")
-        description = (f"<770943114645864470>")
+        description = (f"{role.mention}")
         data.set_image(url=image)
         data.description = "{}".format(description)
         await ctx.send(embed=data)
