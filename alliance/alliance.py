be373a1ed4bede562e38970828800e1ba0adbe35
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-15 08:49:30 +0000

    Indents fix

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -52,34 +52,34 @@ class Alliance(commands.Cog):
                 )
             await ctx.send(embed=embed)
             
-            @commands.group(invoke_without_command=True)
-            async def alliancealert(self, ctx): #aliases: aa, alert):
-                """Alert your fellow alliance mates for movement."""
+    @commands.group(invoke_without_command=True)
+    async def alliancealert(self, ctx): #aliases: aa, alert):
+        """Alert your fellow alliance mates for movement."""
             
-            @commands.group(invoke_without_command=True)
-            async def alliancealertset(self, ctx, role: discord.Role):
-                """Set the alliance role to be pinged for alerts."""
-                await self.config.guild(ctx.guild).role.set(role.id)
-                embed = Embed.create(
-                    self, ctx, title="Successful <:success:777167188816560168>",
-                    description=f"{role.mention} will now be mentioned when Alliance events start.",
-                )
-                try:
-                    await ctx.send(embed=embed)
-                except discord.Forbidden:
-                    embed = Embed.create(
-                        self, ctx, title="Oopsies! <:error:777117297273077760>",
-                        description=f"Something went wrong during the setup process. If this problem continues, please notify Kreusada by using `dem contact <explain your issue`.",
-                    )
-                    await ctx.send(embed=embed)
+    @commands.group(invoke_without_command=True)
+    async def alliancealertset(self, ctx, role: discord.Role):
+        """Set the alliance role to be pinged for alerts."""
+        await self.config.guild(ctx.guild).role.set(role.id)
+        embed = Embed.create(
+            self, ctx, title="Successful <:success:777167188816560168>",
+            description=f"{role.mention} will now be mentioned when Alliance events start.",
+        )
+        try:
+            await ctx.send(embed=embed)
+        except discord.Forbidden:
+            embed = Embed.create(
+                self, ctx, title="Oopsies! <:error:777117297273077760>",
+                description=f"Something went wrong during the setup process. If this problem continues, please notify Kreusada by using `dem contact <explain your issue`.",
+            )
+            await ctx.send(embed=embed)
         
 
-            @alliancealert.command(invoke_without_command=True, pass_context=True, aliases=["aa", "alert"])
-            async def aqstart(self, ctx):
-                """Alliance Quest has started!"""
-                data = Embed.create(self, ctx, title='Alliance Quest has STARTED!')
-                image = ("https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480")
-                description = (f"{role.mention}")
-                data.set_image(url=image)
-                data.description = "{}".format(description)
-                await ctx.send(embed=embed)
+    @alliancealert.command(invoke_without_command=True, pass_context=True, aliases=["aa", "alert"])
+    async def aqstart(self, ctx):
+        """Alliance Quest has started!"""
+        data = Embed.create(self, ctx, title='Alliance Quest has STARTED!')
+        image = ("https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480")
+        description = (f"{role.mention}")
+        data.set_image(url=image)
+        data.description = "{}".format(description)
+        await ctx.send(embed=embed)
