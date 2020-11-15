195282e668ec21429029514c3f1e6158c32b5a7e
Just-Jojo <jojohelp39@gmail.com>
2020-11-15 17:21:50 -0500

    NoneType prevention :D

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -66,7 +66,7 @@ class Alliance(commands.Cog):
     @aa.group(name="set")
     @commands.admin_or_permissions(manage_guild=True)
     async def aas(self, ctx):
-        """Set the alliance role to be pinged for alerts."""
+        """Alliance alert settings"""
 
     @aas.command()
     async def channel(self, ctx, channel: discord.TextChannel):
@@ -98,10 +98,22 @@ class Alliance(commands.Cog):
     @aa.command(invoke_without_command=True, pass_context=True, aliases=["aa", "alert"])
     async def aqstart(self, ctx):
         """Alliance Quest has started!"""
-        embed = Embed.create(
-            self, ctx, title='Alliance Quest has STARTED!',
-            image="https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480",
-            description="Time to join Alliance Quest."
-        )
+
         role = ctx.guild.get_role(await self.config.guild(ctx.guild).get_raw("role"))
-        await ctx.send(content=role.mention, allowed_mentions=discord.AllowedMentions(roles=True), embed=embed)
+        if role is not None:
+            embed = Embed.create(
+                self, ctx, title='Alliance Quest has STARTED!',
+                image="https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480",
+                description="Time to join Alliance Quest."
+            )
+            await ctx.send(content=role.mention, allowed_mentions=discord.AllowedMentions(roles=True), embed=embed)
+        else:
+            embed = Embed.create(
+                self, ctx, title="Error! <:error:777117297273077760>",
+                description=(
+                    "Your guild does not have a role set up for the alerts!\n"
+                    "To set up a role, use `{}alliancealert|aa set role <role>`".format(
+                        ctx.clean_prefix)
+                )
+            )
+            await ctx.send(embed=embed)
