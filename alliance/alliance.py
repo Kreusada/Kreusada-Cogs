75c5a60826a128a8d4d0d53f11e3688bdbd77774
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-15 13:13:05 +0000

    Removed `await` for testing

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -63,7 +63,7 @@ class Alliance(commands.Cog):
     async def alliancealertset(self, ctx, role: discord.Role):
         """Set the alliance role to be pinged for alerts."""
         await self.config.guild(ctx.guild).role.set(role.id)
-        await self.role_mention = role
+        self.role_mention = role
         embed = Embed.create(
             self, ctx, title="Successful <:success:777167188816560168>",
             description=f"{role.mention} will now be mentioned when Alliance events start.",
