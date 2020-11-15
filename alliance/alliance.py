1172603c25779b0519ee2a876b24d0359045869e
Just-Jojo <jojohelp39@gmail.com>
2020-11-15 17:05:19 -0500

    Channel setting

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -3,14 +3,17 @@ from validator_collection import validators
 from redbot.core import commands, checks, Config
 from .allianceembed import Embed
 
+
 class Alliance(commands.Cog):
     """Tools for your alliance on MCOC."""
 
     def __init__(self):
-        self.config = Config.get_conf(self, 200730042020, force_registration=True)
-        default_guild = {"role": None}
-        self.config.register_guild(**default_guild)
-        self.role_mention = None
+        self.config = Config.get_conf(
+            self, 200730042020, force_registration=True)
+        self.config.register_guild(
+            role=None,
+            channel=None
+        )
 
     @commands.command()
     async def timezone(self, ctx, *, timezone: str = None):
@@ -36,14 +39,15 @@ class Alliance(commands.Cog):
         try:
             await user.edit(nick=tag)
             embed = Embed.create(
-                    self, ctx, title="Successful <:success:777167188816560168>",
-                        description="Your timezone is now displayed on your nickname as: ``{}``".format(tag),
+                self, ctx, title="Successful <:success:777167188816560168>",
+                description="Your timezone is now displayed on your nickname as: ``{}``".format(
+                    tag),
             )
             await ctx.send(embed=embed)
         except discord.Forbidden:
             embed = Embed.create(
-                    self, ctx, title="Oopsies! <:error:777117297273077760>",
-                    description="""
+                self, ctx, title="Oopsies! <:error:777117297273077760>",
+                description="""
                     Something went wrong during the setup process, I could not change your nickname.
                     This may be due to the following errors:
                     :x: `Invalid Permissions`
@@ -54,24 +58,35 @@ class Alliance(commands.Cog):
                     """,
             )
             await ctx.send(embed=embed)
-                
-    @commands.group(invoke_without_command=True)
-    async def alliancealert(self, ctx): #aliases: aa, alert):
+
+    @commands.group(name="alliancealert")
+    async def aa(self, ctx):  # aliases: aa, alert):
         """Alert your fellow alliance mates for movement."""
-            
-    @commands.group()
-    async def alliancealertset(self, ctx):
+
+    @aa.group(name="set")
+    @commands.admin_or_permissions(manage_guild=True)
+    async def aas(self, ctx):
         """Set the alliance role to be pinged for alerts."""
-        
-    @alliancealertset.command(invoke_without_command=True)
-    async def role(self, ctx, role: discord.Role):
-        await self.config.guild(ctx.guild).role.set(role.id)
-        self.role_mention = role
+
+    @aas.command()
+    async def channel(self, ctx, channel: discord.TextChannel):
+        await self.config.guild(ctx.guild).channel.set_raw(value=channel)
         embed = Embed.create(
             self, ctx, title="Successful <:success:777167188816560168>",
-            description=f"{role.mention} will now be mentioned when Alliance events start.",
+            description="{} will now be the channel that the alerts will be sent to when Alliance events start".format(
+                channel.mention
+            )
         )
+        await ctx.send(embed=embed)
+
+    @aas.command()
+    async def role(self, ctx, role: discord.Role):
         try:
+            await self.config.guild(ctx.guild).role.set_raw(value=role.id)
+            embed = Embed.create(
+                self, ctx, title="Successful <:success:777167188816560168>",
+                description=f"{role.mention} will now be mentioned when Alliance events start.",
+            )
             await ctx.send(embed=embed)
         except discord.Forbidden:
             embed = Embed.create(
@@ -80,13 +95,13 @@ class Alliance(commands.Cog):
             )
             await ctx.send(embed=embed)
 
-    @alliancealert.command(invoke_without_command=True, pass_context=True, aliases=["aa", "alert"])
+    @aa.command(invoke_without_command=True, pass_context=True, aliases=["aa", "alert"])
     async def aqstart(self, ctx):
         """Alliance Quest has started!"""
         embed = Embed.create(
             self, ctx, title='Alliance Quest has STARTED!',
-            image = "https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480",
-            description = "Time to join Alliance Quest."
+            image="https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480",
+            description="Time to join Alliance Quest."
         )
         role = ctx.guild.get_role(await self.config.guild(ctx.guild).get_raw("role"))
         await ctx.send(content=role.mention, allowed_mentions=discord.AllowedMentions(roles=True), embed=embed)
