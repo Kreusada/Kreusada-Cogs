eae483307d69be205d03943edce7a45d07a0c0ee
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-15 15:04:55 +0000

    Merge

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -89,7 +89,4 @@ class Alliance(commands.Cog):
         image = "https://media.discordapp.net/attachments/745608075670585344/772947661421805648/aqstarted.png?width=1441&height=480",
         description = "Time to join Alliance Quest."
         )
-#        data.set_image(url=image)
-#        data.description = "{}".format(description)
-        await ctx.send(embed=embed)
-        await ctx.send(f"{self.role_mention}")
+        await ctx.send(f"{self.role_mention}", embed=embed)
