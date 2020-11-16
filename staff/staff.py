b814e98a0c0b56f945239ce49d057dc02429f413
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-16 18:53:47 +0000

    Removed datetime moduler for now

--- staff/staff.py
+++ staff/staff.py
@@ -68,8 +68,8 @@ class Staff(commands.Cog):
         if role is not None:
             embed = Embed.create(
                 self, ctx, title='<:alert:777928824670388254> ALERT!',
-                description=f"**{ctx.author}** has just called for the staff in {ctx.channel.mention}.\n\n**Requested at: {} on {}**"
-                embed.set_footer(datetime.now(timezone.utc))  
+                description=f"**{ctx.author}** has just called for the staff in {ctx.channel.mention}."
+#                embed.set_footer(datetime.now(timezone.utc))  
             )
             await channel.send(content=role.mention, allowed_mentions=discord.AllowedMentions(roles=True), embed=embed)
         else:
