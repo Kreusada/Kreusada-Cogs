07fca747853d5971885e21c1fc70387b734737c8
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-17 10:36:17 +0000

    Fixes

--- kreustools/kreustools.py
+++ kreustools/kreustools.py
@@ -22,7 +22,6 @@ class Kreustools(commands.Cog):
       channel = ctx.message.channel
       topic = channel.topic
       if topic is not None and topic != '':
-        data = Embed.create(ctx, title=f"#{channel.name} Topic :star:",
+        embed = Embed.create(self, ctx, title=f"#{channel.name} Topic :star:",
                             description=topic)
-        data.set_thumbnail(url=ctx.message.guild.icon_url)
-        await ctx.send(embed=data)
+        await ctx.send(embed=embed)
