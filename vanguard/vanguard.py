fcc437cfbcfa6324dc698affbae91bc51ba42356
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-17 19:14:23 +0000

    Placeholding

--- vanguard/vanguard.py
+++ vanguard/vanguard.py
@@ -1,4 +1,46 @@
 import discord
 from validator_collection import validators
 from redbot.core import commands, checks, Config, modlog
-from .staffembed import Embed
+from .vangem import Embed
+
+class Vanguard(commands.Cog):
+  """Vanguard Support Schema."""
+  
+  def __init__(self,bot):
+      self.bot = bot
+      self.vg = self.bot.get_guild(744572173137477692)
+      self.config = Config.get_conf(
+          self, 200730042020, force_registration=True)
+      default_guild = {
+          "role": None,
+          "channel": None
+      }
+      self.config.register_guild(**default_guild)
+      
+  @commands.group()
+  async def rule(self, ctx):
+    """Vanguard Rule Index"""
+    
+  @commands.group()
+  async def guide(self, ctx):
+    """Vanguard Guide Index"""
+    
+  @rule.command()
+  @checks.admin()
+  async def full(self, ctx):
+    embed = create.Embed(
+      self, ctx, title="<:alert:777928824670388254> Vanguard Rules",
+      description=(
+        "Hey there, welcome to the Vanguards. "
+        "This server can help you to learn, implement and use **code**. "
+        "We support multiple coding languages and are happy to help you. "
+        "Additionally, this is the primary support server for <@766580519000473640>. "
+        "We have some rules, which **must** be followed for the welfare of our members.\n"
+        "**Rule 1**\nDO NOT share bot tokens anywhere. If you're showing code which includes the token, please replace it with placeholding text.\n"
+        "**Rule 2**\nKeep to the correct channels, we'll be strict on this. If you don't know where you're going, ask in <#758775858222727168>. Additionally, keep discussion relevant to the channel topic.\n" 
+        "**Rule 3**\nWe will not offer support for coding which involves illegally mitigating APIs and websites, or that breaks terms of service for discord or the targeted environment. Or for other actions which may be considered malicious from intent.\n"
+        "**Rule 4**\nListen to and respect staff members and their instructions. Do not be rude or vulgar to those who voluntarily give up their time to help you.\n"
+        "**Rule 5""\nDo not spam. Do not advertise content; this includes discord servers, or media that you are looking to promote. We don't do that here.\n"
+        "**Rule 6""\nUsernames that involve unicode, slurs or invisibility will be changed."
+      )
+      await ctx.send(embed=embed)
