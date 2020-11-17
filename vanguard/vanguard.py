ce5aa3aab6b5a7f5592f94678265ff08772f991a
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-17 19:39:41 +0000

    Testing

--- vanguard/vanguard.py
+++ vanguard/vanguard.py
@@ -35,13 +35,14 @@ class Vanguard(commands.Cog):
         "This server can help you to learn, implement and use **code**. "
         "We support multiple coding languages and are happy to help you. "
         "Additionally, this is the primary support server for <@766580519000473640>. "
-        "We have some rules, which **must** be followed for the welfare of our members.\n"
-        "**Rule 1**\nDO NOT share bot tokens anywhere. If you're showing code which includes the token, please replace it with placeholding text.\n"
-        "**Rule 2**\nKeep to the correct channels, we'll be strict on this. If you don't know where you're going, ask in <#758775858222727168>. Additionally, keep discussion relevant to the channel topic.\n" 
-        "**Rule 3**\nWe will not offer support for coding which involves illegally mitigating APIs and websites, or that breaks terms of service for discord or the targeted environment. Or for other actions which may be considered malicious from intent.\n"
-        "**Rule 4**\nListen to and respect staff members and their instructions. Do not be rude or vulgar to those who voluntarily give up their time to help you.\n"
-        "**Rule 5""\nDo not spam. Do not advertise content; this includes discord servers, or media that you are looking to promote. We don't do that here.\n"
-        "**Rule 6""\nUsernames that involve unicode, slurs or invisibility will be changed."
+        "We have some rules, which **must** be followed for the welfare of our members.\n\n"
+        "**Rule 1**\n\nDO NOT share bot tokens anywhere. If you're showing code which includes the token, please replace it with placeholding text.\n\n"
+        "**Rule 2**\n\nKeep to the correct channels, we'll be strict on this. If you don't know where you're going, ask in <#758775858222727168>. Additionally, keep discussion relevant to the channel topic.\n\n" 
+        "**Rule 3**\n\nWe will not offer support for coding which involves illegally mitigating APIs and websites, or that breaks terms of service for discord or the targeted environment. Or for other actions which may be considered malicious from intent.\n\n"
+        "**Rule 4**\n\nListen to and respect staff members and their instructions. Do not be rude or vulgar to those who voluntarily give up their time to help you.\n\n"
+        "**Rule 5**\n\nDo not spam. Do not advertise content; this includes discord servers, or media that you are looking to promote. We don't do that here.\n\n"
+        "**Rule 6""\n\nUsernames that involve unicode, slurs or invisibility will be changed."
       )
+      color=0x59e1ac
     )
     await ctx.send(embed=embed)
