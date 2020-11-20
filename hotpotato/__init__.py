955aaac39453f3f3597d7e0bdeca59f82ee66e04
kreus7 <67752638+kreus7@users.noreply.github.com>
2020-11-20 09:16:35 +0000

    Moved to current repository

---
+++ hotpotato/__init__.py
@@ -0,0 +1,7 @@
+from .hotpotato import HotPotato
+
+__red_end_user_data_statement__ = "This cog stores Discord IDs as needed for operation temporarily during the game which are automatically deleted when it ends."
+
+
+def setup(bot):
+    bot.add_cog(HotPotato())
