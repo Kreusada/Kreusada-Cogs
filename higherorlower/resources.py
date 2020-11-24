0ba238af7ca4598883af259d3ae5f185768a925e
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-24 13:52:10 +0000

    Update resources.py

--- higherorlower/resources.py
+++ higherorlower/resources.py
@@ -11,18 +11,8 @@ class Embed:
 
     def create(self, ctx, color=discord.Color.red(), title='', description='', image=None,
                thumbnail=None, url=None, footer_text=None, footer_url=None, author_text=None):
-        '''Return a color styled embed with MDT footer, and optional title or description.
-        user_id = user id string. If none provided, takes message author.
-        color = manual override, otherwise takes gold for private channels, or author color for guild.
-        title = String, sets title.
-        description = String, sets description.
-        image = String url.  Validator checks for valid url.
-        thumbnail = String url. Validator checks for valid url.'''
-        CRYSTAL = 'https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/f/fe/6-Star_Crystal.png/revision/latest?cb=20200605225602'
-        PATREON = 'https://patreon.com/matrixdt'
-        MDT_LOGO = 'https://cdn.discordapp.com/attachments/745608075670585344/767361113477611580/MatrixDevelopmentTeam.png'
+        
         DEMARATUS = 'https://cdn.discordapp.com/attachments/758775890954944572/768452440785027132/demaratuscircle.png'
-
         if isinstance(ctx.message.channel, discord.abc.GuildChannel):
             color = ctx.message.author.color
         data = discord.Embed(color=color, title=title, url=url)
