20477d85dc58a11fac003766ace0d825f07c05f0
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-24 18:42:56 +0000

    Placeholding

---
+++ robthebank/robthebank.py
@@ -0,0 +1,9 @@
+import discord
+import random
+import asyncio
+from random import randint
+from discord.utils import get
+from redbot.core import commands, checks, bank, Config
+from redbot.core.utils.predicates import MessagePredicate
+
+class RobTheBank(commands.Cog):
