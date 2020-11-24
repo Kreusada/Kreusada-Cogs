80df68e1a259167120eac1e9eed93513731b840d
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-24 20:17:51 +0000

    will come back to this later

--- robthebank/robthebank.py
+++ robthebank/robthebank.py
@@ -9,15 +9,24 @@ from redbot.core.utils.predicates import MessagePredicate
 
 class RobTheBank(commands.Cog):
   """Rob the bank, gain or get fined!"""
+  defaults = {
+    "Fine": 400,
+    "Deposti": 400,
+  }
 
   def __init__(self, bot):
     self.bot = bot
     self.config = Config.get_conf(
       self, identifier=5865146514315491, force_registration=True
     )
+    self.config.register_guild(**self.defaults)
+
+  async def red_delete_data_for_user(self, **kwargs):
+      """Nothing to delete."""
+      return
    
   @commands.command()
-  @commands.cooldown(1, 300, BucketType)
+  @commands.cooldown(1, 300, commands.BucketType.user)
   async def rob(self, ctx):
     """Attempt to rob the bank. Attempt."""
     resu = random.randint(RESU)
@@ -25,9 +34,20 @@ class RobTheBank(commands.Cog):
     if resu > 4:
       title = status
       description = random.choice(UNSUCRESP)
+      await bank.withdraw_credits(ctx.author, settings["Fine"])
     else:
       title = status
       description = random.choice(SUCRESP)
+      await bank.deposit_credits(ctx.author, settings["Deposti"])
     data = Embed.create(self, ctx, title=title, description=description)
+    await ctx.send(embed=data)
+    
+  @commands.group()
+  async def rtbset(self, ctx):
+    """Configurations for robbing the bank."""
+    
+  @rtbset.command()
+  async def deposit(self, ctx, amount: int)
+    
       
       
