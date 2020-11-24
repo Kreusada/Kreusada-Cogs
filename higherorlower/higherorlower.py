2a76ca1dfb0b0d7e3f3e0cd9e5b6d63e076ff578
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-24 10:30:29 +0000

    Testing

--- higherorlower/higherorlower.py
+++ higherorlower/higherorlower.py
@@ -16,43 +16,43 @@ class HigherOrLower(commands.Cog):
       self, identifier=5865146514315491, force_registration=True
     )
     
-  @commands.command()
-  async def holset(self, ctx):
-    """Configuration for Higher or Lower"""
-    currency = await bank.get_currency_name(ctx.guild)
-    pred = MessagePredicate.yes_or_no(ctx)
-    await ctx.send("Do you want the winner to get a role?\n Please type `yes` or `no`.")
-    try:
-      await self.bot.wait_for("message", timeout=30, check=pred)
-    except asyncio.TimeoutError:
-      return await ctx.send("You took too long there.")
-    if pred.result:
-      await ctx.send("What role should it be?")
-      role = MessagePredicate.valid_role(ctx)
-      try:
-        await self.bot.wait_for("message", timeout=30, check=role)
-      except asyncio.TimeoutError:
-        return await ctx.send("You took too long there.")
-      required = role.result
-      await self.config.guild(ctx.guild).required.set(str(required))
-      await ctx.send(
-        f"How much {currency} do you want winners to receive?"
-      )
-      predi = MessagePredicate.valid_int(ctx)
-      try:
-          await self.bot.wait_for("message", timeout=30, check=predi)
-      except asyncio.TimeoutError:
-        return await ctx.send("You took too long there.")
-      amount = predi.result
-      await self.config.guild(ctx.guild).amount.set(amount)
-      await ctx.send("Your currency payout has been set.")
-      
+#  @commands.command()
+#  async def holset(self, ctx):
+#    """Configuration for Higher or Lower"""
+#    currency = await bank.get_currency_name(ctx.guild)
+#    pred = MessagePredicate.yes_or_no(ctx)
+#    await ctx.send("Do you want the winner to get a role?\n Please type `yes` or `no`.")
+#    try:
+#      await self.bot.wait_for("message", timeout=30, check=pred)
+#    except asyncio.TimeoutError:
+#      return await ctx.send("You took too long there.")
+#    if pred.result:
+#      await ctx.send("What role should it be?")
+#      role = MessagePredicate.valid_role(ctx)
+#      try:
+#        await self.bot.wait_for("message", timeout=30, check=role)
+#      except asyncio.TimeoutError:
+#        return await ctx.send("You took too long there.")
+#      required = role.result
+#      await self.config.guild(ctx.guild).required.set(str(required))
+#      await ctx.send(
+#        f"How much {currency} do you want winners to receive?"
+#      )
+#      predi = MessagePredicate.valid_int(ctx)
+#      try:
+#          await self.bot.wait_for("message", timeout=30, check=predi)
+#      except asyncio.TimeoutError:
+#        return await ctx.send("You took too long there.")
+#      amount = predi.result
+#      await self.config.guild(ctx.guild).amount.set(amount)
+#      await ctx.send("Your currency payout has been set.")
+#      
   @commands.command(aliases = ["hol"])
   async def higherorlower(self, ctx):
-    currency = await bank.get_currency_name(ctx.guild)
-    name_required = await self.config.guild(ctx.guild).required()
-    required = get(ctx.guild.roles, name=name_required)
-    amount = await self.config.guild(ctx.guild).amount()
+#    currency = await bank.get_currency_name(ctx.guild)
+#    name_required = await self.config.guild(ctx.guild).required()
+#    required = get(ctx.guild.roles, name=name_required)
+#    amount = await self.config.guild(ctx.guild).amount()
     for x in range(1, 15):
       random.choice(x)
     if x == 1:
