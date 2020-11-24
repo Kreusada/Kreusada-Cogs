4a39763f51b736b0c1e8cc6593bde717e3ba9e36
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-24 19:12:24 +0000

    Update robthebank.py

--- robthebank/robthebank.py
+++ robthebank/robthebank.py
@@ -2,8 +2,32 @@ import discord
 import random
 import asyncio
 from random import randint
+from .rtbresources import Embed
 from discord.utils import get
 from redbot.core import commands, checks, bank, Config
 from redbot.core.utils.predicates import MessagePredicate
 
 class RobTheBank(commands.Cog):
+  """Rob the bank, gain or get fined!"""
+
+  def __init__(self, bot):
+    self.bot = bot
+    self.config = Config.get_conf(
+      self, identifier=5865146514315491, force_registration=True
+    )
+   
+  @commands.command()
+  @commands.cooldown(1, 300, BucketType)
+  async def rob(self, ctx):
+    """Attempt to rob the bank. Attempt."""
+    resu = random.randint(RESU)
+    status = f"{ctx.author.name} attempted to rob the bank..."
+    if resu > 4:
+      title = status
+      description = random.choice(UNSUCRESP)
+    else:
+      title = status
+      description = random.choice(SUCRESP)
+    data = Embed.create(self, ctx, title=title, description=description)
+      
+      
