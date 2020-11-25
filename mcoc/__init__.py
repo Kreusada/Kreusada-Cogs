d83a8bddff689227a0a149cd6430236e1f464363
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-25 15:28:07 +0000

    Added red end user statement

--- mcoc/__init__.py
+++ mcoc/__init__.py
@@ -1,5 +1,11 @@
+from redbot.core.bot import Red
 from .mcoc import Mcoc
 
-async def setup(bot):
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+def setup(bot: Red):
     cog = Mcoc(bot)
     bot.add_cog(cog)
