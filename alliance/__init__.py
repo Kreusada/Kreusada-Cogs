d5cf8c744235a3f8b1598d5722d80f4bc1dc058a
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-25 15:21:45 +0000

    Added red end user statement

--- alliance/__init__.py
+++ alliance/__init__.py
@@ -1,4 +1,11 @@
+from redbot.core.bot import Red
 from .alliance import Alliance
 
-def setup(bot):
-    bot.add_cog(Alliance())
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+def setup(bot: Red):
+    cog = Alliance(bot)
+    bot.add_cog(cog)
