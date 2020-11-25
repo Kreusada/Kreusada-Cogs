89d810df5f072d04b7d22c39c341ed1c170f420c
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-25 12:44:12 +0000

    Added red end user statement

--- robthebank/__init__.py
+++ robthebank/__init__.py
@@ -1,4 +1,11 @@
+from redbot.core.bot import Red
 from .robthebank import RobTheBank
 
-def setup(bot):
-    bot.add_cog(RobTheBank(bot))
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+def setup(bot: Red):
+    cog = RobTheBank(bot)
+    bot.add_cog(cog)
