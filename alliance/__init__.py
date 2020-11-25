6fc030f875cbc52f69cecd6067ce7ee756232e78
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-25 15:42:30 +0000

    Fixes

--- alliance/__init__.py
+++ alliance/__init__.py
@@ -1,10 +1,8 @@
 from redbot.core.bot import Red
 from .alliance import Alliance
 
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
+__red_end_user_data_statement__ = ("This cog does not persistently store data or metadata about users.")
 
-
-def setup(bot: Red):
-    bot.add_cog(Alliance)
+def setup(bot):
+    cog = Alliance(bot)
+    bot.add_cog(cog)
