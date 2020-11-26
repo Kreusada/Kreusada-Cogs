bbf6763ac2379a1d946d02d5da01814a09571a83
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-26 19:57:10 +0000

    Removed formatting

--- gifts/gdicts.py
+++ gifts/gdicts.py
@@ -31,13 +31,13 @@ class Embed:
 
 COLLS = [
   "Wow, look at all these beautiful gifts!",
-  "Consider yourself lucky, {}!".format(author.name),
+  "Consider yourself lucky!"
   "These are your gifts? May I have some? :pleading_face:"
 ]
 
 COLLN = [
   "Oh no! No gifts?! This is a tragedy.",
-  "Get yourself some free gifts {}!".format(author.name),
+  "Get yourself some free gifts!"
   "This is not a nice vibe. Where are those gifts!"
 ]
 
