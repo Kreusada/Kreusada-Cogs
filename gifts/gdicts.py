7c7221004d10dc7991e9a755d300933c075577fc
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-26 16:18:44 +0000

    Tests

--- gifts/gdicts.py
+++ gifts/gdicts.py
@@ -1,1 +1,16 @@
 COLLS = [
+  "Wow, look at all these beautiful gifts!",
+  "Consider yourself lucky, {}!".format(ctx.author.name),
+  "These are your gifts? May I have some? :pleading_face:"
+]
+
+COLLN = [
+  "Oh no! No gifts?! This is a tragedy.",
+  "Get yourself some free gifts {}!".format(ctx.author.name),
+  "This is not a nice vibe. Where are those gifts!"
+]
+
+GIFTS = {
+  "sock": "https://lh3.googleusercontent.com/proxy/LE92TgJSMnQoLad6P0q20KGR0hZf9Ouo6VCOGwNPn5OkJdWQESBe943oRsQzVBUs-C-l34wJLkSBFfzZmlI3s2POJN8U7B3CnE3DXSAikGWKihuKEi5d",
+  "cookie": "https://assets.stickpng.com/images/580b57fbd9996e24bc43c0fc.png"
+}
