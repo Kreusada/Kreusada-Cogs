ced822a612933986b16470f5c4e3f1cced358451
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-26 16:20:44 +0000

    embed updates

--- gifts/gdicts.py
+++ gifts/gdicts.py
@@ -1,3 +1,34 @@
+import discord
+import requests
+from validator_collection import validators
+
+class Embed:
+    def __init__(self, bot):
+        self.bot = bot
+
+    def create(self, ctx, color=discord.Color.red(), title='', description='', image=None,
+               thumbnail=None, url=None, footer_text=None, footer_url=None, author_text=None):
+        if isinstance(ctx.message.channel, discord.abc.GuildChannel):
+            color = ctx.message.author.color
+        data = discord.Embed(color=color, title=title, url=url)
+        if description is not None:
+            if len(description) < 1500:
+                data.description = description
+        data.set_author(name=ctx.message.author.display_name,
+                        icon_url=ctx.message.author.avatar_url)
+        if image is not None:
+            validators.url(image)
+            code = requests.get(image).status_code
+            if code == 200:
+                data.set_image(url=image)
+            else:
+                print('Image URL Failure, code {}'.format(code))
+                print('Attempted URL:\n{}'.format(image))
+        if footer_text is None:
+            footer_text = "Gifts"
+        data.set_footer(text=footer_text)
+        return data
+
 COLLS = [
   "Wow, look at all these beautiful gifts!",
   "Consider yourself lucky, {}!".format(ctx.author.name),
