8083a66c606737545fa3ff10029a3c3d7cc029cf
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-26 21:35:56 +0000

    Now mentioning champion name from crystal

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -54,7 +54,7 @@ class Mcoc(commands.Cog):
         roster = await self.config.user(ctx.author).roster.get_raw()
         await self.roster_logistics(ctx, star, key, roster)
         data = Embed.create(
-            self, ctx, title='You got... <:crystal:776941489464672266>',
+            self, ctx, title='You got {}'.format(key),
             description="⭐" * int(star)
         )
         data.set_image(url=image)
