e7c6cfe24623f3ffbbbe61f3bf1e83f7d508bb7c
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-26 19:56:29 +0000

    ctx fixes

--- gifts/gdicts.py
+++ gifts/gdicts.py
@@ -31,13 +31,13 @@ class Embed:
 
 COLLS = [
   "Wow, look at all these beautiful gifts!",
-  "Consider yourself lucky, {}!".format(ctx.author.name),
+  "Consider yourself lucky, {}!".format(author.name),
   "These are your gifts? May I have some? :pleading_face:"
 ]
 
 COLLN = [
   "Oh no! No gifts?! This is a tragedy.",
-  "Get yourself some free gifts {}!".format(ctx.author.name),
+  "Get yourself some free gifts {}!".format(author.name),
   "This is not a nice vibe. Where are those gifts!"
 ]
 
