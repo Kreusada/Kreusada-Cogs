bb15f595f302838d6e1536a2d0a3eee475b5e1dc
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-27 20:16:48 +0000

    Create __init__.py

---
+++ mjolnir/__init__.py
@@ -0,0 +1,10 @@
+from .mjolnir import Mjolnir
+
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+def setup(bot):
+    cog = Mjolnir(bot)
+    bot.add_cog(cog)
