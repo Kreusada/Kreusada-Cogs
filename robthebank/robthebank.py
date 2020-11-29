06f40578550d835dbeb0bea4ac06413d45807a11
Just-Jojo <jojohelp39@gmail.com>
2020-11-28 22:31:56 -0500

    Total rewrite of the Rob the Bank module :D

--- robthebank/robthebank.py
+++ robthebank/robthebank.py
@@ -1,120 +1,125 @@
 import discord
 import random
 import asyncio
-from random import randint
-from discord.utils import get
-from redbot.core import commands, checks, bank, Config
-from redbot.core.utils.predicates import MessagePredicate
-from .rtbresources import Embed
+
+from redbot.core import (
+    commands, bank, Config
+)
+
+
+# , **kwargs: dict):
+def yes_no_returner(fail: bool, ctx: commands.Context) -> str:
+    yes = [
+        ":loudspeaker: Dispatch, we've lost the suspect.\n",
+        f"Looks like {ctx.author.name} made it out alive, somehow...\n",
+        "We let you loose on purpose, we really did.\n"
+    ]
+    no = [
+        "Oh I caught you red handed there!\n",
+        "Get some good detective skills before trying to rob my bank!\n",
+        f"Oh, it's you again... {ctx.author.name} is it?\n",
+        "Ladies and gentlemen, we gottem.\n"
+    ]
+    return random.choice(yes) if fail else random.choice(no)
+
 
 class RobTheBank(commands.Cog):
-  """Rob the bank, gain or get fined!"""
-  defaults = {
-    "Fine": 400,
-    "Deposit": 400,
-  }
+    """Rob the bank. Gain or get fired"""
 
-  def __init__(self, bot):
-    self.bot = bot
-    self.config = Config.get_conf(
-      self, identifier=5865146514315491, force_registration=True
-    )
-    self.config.register_guild(**self.defaults)
+    def __init__(self, bot):
+        default_guild = {
+            "Fine": 400,
+            "Deposit": 400
+        }
+        self.bot = bot
+        self.embed = Embed(self.bot)
+        self.config = Config.get_conf(
+            self, identifier=5865146514315491, force_registration=True
+        )
+        self.config.register_guild(
+            **default_guild
+        )
 
-  async def red_delete_data_for_user(self, **kwargs):
-      """Nothing to delete."""
-      return
-   
-  @commands.command()
-#  @commands.cooldown(1, 300, commands.BucketType.user)
-  async def rob(self, ctx):
-    """Attempt to rob the bank. Attempt."""
-    settings = await self.config.guild(ctx.guild).all()
-    numbers = [1,2,3,4,5,6]
-    numbersrand = random.choice(numbers)
-    yestatus = f"<:dollarbag:778687019944771616> {ctx.author.name} successfully robbed the bank."
-    nostatus = f"<:dollarbag:778687019944771616> {ctx.author.name} failed, dismally."
-    bankrupt = f"Oh no {ctx.author.name}! You were fined more than the cash you have!"
-    currency = await bank.get_currency_name(ctx.guild)
-    deposit = await self.config.guild(ctx.guild).Deposit()
-    fine = await self.config.guild(ctx.guild).Fine()
-    subt = f"<:success:777167188816560168> {fine} {currency} has been withdrawn from your bank account."
-    incr = f"<:success:777167188816560168> {deposit} {currency} has been added into your bank account."
-    no = [
-      f"Oh I caught you red handed there!\n **You have been fined {fine} {currency}.**\n\n {subt}",
-      f"Get some good detective skills before trying to rob my bank!\n **You have been fined {fine} {currency}.**\n\n {subt}",
-      f"Oh, its you again... {ctx.author.name} is it?\n **You have been fined {fine} {currency}.**\n\n {subt}",
-      f"Ladies and gentlemen, we gottem.\n **You have been fined {fine} {currency}.**\n\n {subt}"
-    ]
-    yes = [
-      f":loudspeaker: Dispatch, we've lost the suspect.\n **You have earnt yourself {deposit} {currency}!**\n\n {incr}",
-      f"Looks like {ctx.author.name} made it out alive, somehow...\n **You have earnt yourself {deposit} {currency}!**\n\n {incr}",
-      f"We let you loose on purpose, we really did.\n **You have earnt yourself {deposit} {currency}!**\n\n {incr}"
-    ]
-    if numbersrand > 4:
-      title = nostatus
-      description = random.choice(no).format(currency)
-    try:
-      await bank.withdraw_credits(ctx.author, settings["Fine"])
-    except ValueError:
-      await ctx.send(bankrupt)
-    else:
-      title = yestatus
-      description = random.choice(yes).format(currency)
-      await bank.deposit_credits(ctx.author, settings["Deposit"])
-    data = Embed.create(self, ctx, title=title, description=description)
-    await ctx.send(embed=data)
-    
-  @commands.group()
-  @checks.mod()
-  async def robset(self, ctx):
-    """Configurations for robbing the bank."""
-    
-  @robset.command()
-  async def deposit(self, ctx, amount: int):
-    """
-    Set the amount you can steal from the bank.
-    Odds for and against acquiring money are 50/50.
-    The default value for deposits are: `400`.
-    We don't recommend going much higher than this.
-    """
-    if amount < 0:
-      return await ctx.send("You wouldn't be robbing a bank if you didn't get any money!\n"
-                            "Please enter a number equal to or greater than `1`.")
-    await self.config.guild(ctx.guild).Deposit.set(amount)
-    currency = await bank.get_currency_name(ctx.guild)
-    await ctx.send(f"Deposits will now distribute **{amount} {currency}.**")
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
 
-  @robset.command()
-  async def fine(self, ctx, amount: int):
-    """
-    Set the amount you can get fined for from the bank.
-    Odds for and against acquiring money are 50/50.
-    The default value for fines are: `400`.
-    We don't recommend going much higher than this.
-    """
-    if amount < 0:
-      return await ctx.send("That's not how fines work :joy:\nPlease enter a number equal to or greater than `1`.")
-    await self.config.guild(ctx.guild).Fine.set(amount)
-    currency = await bank.get_currency_name(ctx.guild)
-    await ctx.send(f"Fines will now withdraw **{amount} {currency}.**")
-    
-  @robset.command()
-  async def showsettings(self, ctx):
-    """Shows the current settings for RobTheBank."""
-    fin = await self.config.guild(ctx.guild).Fine()
-    deposi = await self.config.guild(ctx.guild).Deposit()
-    if fin == 400 and deposi == 400:
-      s = "Standard"
-    else:
-      s = "Customized"
-    embed = Embed.create(self, ctx, title=f"{ctx.guild.name} Settings",
-                         description=(
-                           f"Deposit: **{deposi}**\n"
-                           f"Fine: **{fin}**\n"
-                           f"Settings: **{s}**"
-                         )
-                        )
-    await ctx.send(embed=embed)
-      
-      
+    @commands.command()
+    async def rob(self, ctx: commands.Context):
+        """Attempt to rob the bank... **Attempt**"""
+        if random.randint(1, 6) > 4:
+            fine = await self.config.guild(ctx.guild).get_raw("Fine")
+            description = yes_no_returner(
+                False, ctx) + "**You have been fined {}**".format(fine)
+            title = f"<:dollarbag:778687019944771616> {ctx.author.name} failed, dismally."
+            try:
+                await bank.withdraw_credits(ctx.author, fine)
+            except ValueError:
+                await ctx.send("Oh no! You were fined more cash than you have!")
+        else:
+            deposit = await self.config.guild(ctx.guild).get_raw("Deposit")
+            description = yes_no_returner(True, ctx) + \
+                "**You have earnt yourself {}!**".format(deposit)
+            title = f"<:dollarbag:778687019944771616> {ctx.author.name} successfully robbed the bank."
+            await bank.deposit_credits(ctx.author, deposit)
+        embed = self.embed.create(ctx, title=title, description=description)
+        await ctx.send(embed=embed)
+
+    @commands.group(name="robset")
+    @commands.mod()
+    async def rob_set(self, ctx: commands.Context):
+        """Configurations for robbing the bank"""
+
+    @rob_set.command()
+    async def deposit(self, ctx: commands.Context, amount: int):
+        """Set the amount a user can steal"""
+        if amount < 1:
+            await ctx.send("No point in stealing from a bank if you're not going to get anything from it")
+        cur = await bank.get_currency_name(ctx.guild)
+
+        await self.config.guild(ctx.guild).set_raw("Deposit", value=amount)
+        await ctx.send("Deposits will now give out {} {}".format(
+            amount, cur
+        ))
+
+    @rob_set.command()
+    async def fine(self, ctx: commands.Context, amount: int):
+        """Set the fine amount"""
+        if amount < 1:
+            await ctx.send("You might as well not be fined!")
+        cur = await bank.get_currency_name(ctx.guild)
+
+        await self.config.guild(ctx.guild).set_raw("Fine", value=amount)
+        await ctx.send("Fines will now withdraw {} {}".format(amount, cur))
+
+    @rob_set.command()
+    async def show_settings(self, ctx: commands.Context):
+        """Show the current settings for Rob the bank"""
+        fine = await self.config.guild(ctx.guild).get_raw("Fine")
+        deposit = await self.config.guild(ctx.guild).get_raw("Deposit")
+        embed = self.embed.create(
+            ctx, title="{}'s Settings".format(ctx.guild.name))
+        embed.add_field(name="Fine", value=fine)
+        embed.add_field(name="Deposit", value=deposit)
+        await ctx.send(embed=embed)
+
+
+class Embed:
+    def __init__(self, bot):
+        self.bot = bot
+
+    def create(self, ctx, color=discord.Color.red(), title='', description='', image=None,
+               thumbanil: str = None) -> discord.Embed:
+        if isinstance(ctx.message.channel, discord.abc.GuildChannel):
+            color = ctx.message.author.color
+        data = discord.Embed(color=color, title=title)  # , url=url)
+        if description is not None:
+            if len(description) < 1500:
+                data.description = description
+        data.set_author(
+            name=ctx.author.display_name,
+            icon_url=ctx.message.author.avatar_url
+        )
+        if image is not None:
+            data.set_image(url=image)
+        return data
