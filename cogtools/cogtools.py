342ac53a81db4b7457f321db5e4cfeb72a635d78
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-30 21:32:20 +0000

    Update cogtools.py

--- cogtools/cogtools.py
+++ cogtools/cogtools.py
@@ -2,20 +2,16 @@ import discord
 from redbot.core import commands, checks
 from redbot.cogs.downloader.converters import InstalledCog
 
-class CogTools(commands.Cog):
-  """Tools for cogs."""
+class l(commands.Cog):
+  """private learning"""
   
   def __init__(self, bot):
     self.bot = bot
   
-    @commands.command()
-    @commands.max_concurrency(1)
-    @commands.is_owner()
-    async def updreload(self, ctx: commands.Context):
-        """Updates installed cogs and reloads the loaded ones automatically."""
-        ctx.assume_yes = True
-        cog_upd_command = ctx.bot.get_command("cog update")
-        if cog_upd_command is None:
-            await ctx.send("Welp, that ain't gonna happen. \nYou need to `{}load downloader` first.".format(ctx.prefix))
-        else:
-            await ctx.invoke(cog_upd_command)
+    @bot.event
+    async def on_message(message):
+      if message.author == bot.user:
+        return
+      
+      if message.content.startswith("Demaratus"):
+        await message.channel.send("Hi")
