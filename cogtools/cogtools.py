1ef68d4dcf7e0c64587f10e477e5cdb019cff40e
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-30 21:21:46 +0000

    Update cogtools.py

--- cogtools/cogtools.py
+++ cogtools/cogtools.py
@@ -3,17 +3,19 @@ from redbot.core import commands, checks
 from redbot.cogs.downloader.converters import InstalledCog
 
 class Cogtools(commands.Cog):
+  """Tools for cogs."""
   
-  @commands.is_owner()
-  @commands.command()
-  async def updr(ctx, *cogs: InstalledCog):
-    """Update cogs without questioning about reload."""
-    ctx.assume_yes = True
-    cog_update_command = ctx.bot.get_command("cog update")
-    if cog_update_command is None:
-        await ctx.send(
-          f"I can't find {ctx.prefix}cog update command"
-        )
-        return
-    await ctx.invoke(cog_update_command, *cogs)
-    return updr
+  def __init__(self, bot):
+    self.bot = bot
+  
+    @commands.command()
+    @commands.max_concurrency(1)
+    @commands.is_owner()
+    async def updreload(self, ctx: commands.Context):
+        """Updates installed cogs and reloads the loaded ones automatically."""
+        ctx.assume_yes = True
+        cog_upd_command = ctx.bot.get_command("cog update")
+        if cog_upd_command is None:
+            await ctx.send("Welp, that ain't gonna happen. \nYou need to `{}load downloader` first.".format(ctx.prefix))
+        else:
+            await ctx.invoke(cog_upd_command)
