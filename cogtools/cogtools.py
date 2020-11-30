f9563d9740e64d85c698a04eccd2fcd3cb7d859a
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-30 21:14:39 +0000

    Update cogtools.py

--- cogtools/cogtools.py
+++ cogtools/cogtools.py
@@ -8,13 +8,13 @@ class Cogtools(commands.Cog):
   @commands.is_owner()
   @commands.command()
   async def updr(ctx, *cogs: InstalledCog):
-     """Update cogs without questioning about reload."""
-      ctx.assume_yes = True
-      cog_update_command = ctx.bot.get_command("cog update")
-      if cog_update_command is None:
-          await ctx.send(
-            f"I can't find {ctx.prefix}cog update command"
-          )
-          return
-      await ctx.invoke(cog_update_command, *cogs)
-  return updr
+    """Update cogs without questioning about reload."""
+    ctx.assume_yes = True
+    cog_update_command = ctx.bot.get_command("cog update")
+    if cog_update_command is None:
+        await ctx.send(
+          f"I can't find {ctx.prefix}cog update command"
+        )
+        return
+    await ctx.invoke(cog_update_command, *cogs)
+    return updr
