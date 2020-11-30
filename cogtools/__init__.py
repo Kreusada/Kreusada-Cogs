8662a479cf58a5907b28d786dcff1c64b70242dc
kreusada <67752638+kreus7@users.noreply.github.com>
2020-11-30 21:16:41 +0000

    Update __init__.py

--- cogtools/__init__.py
+++ cogtools/__init__.py
@@ -1,4 +1,11 @@
+from redbot.core.bot import Red
 from .cogtools import Cogtools
 
-def setup(bot):
-  bot.add_cog(Cogtools)
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+def setup(bot: Red):
+    cog = Cogtools(bot)
+    bot.add_cog(cog)
