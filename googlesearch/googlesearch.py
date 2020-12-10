52ed32058724c8dbb317f1bd83c3331eff2acc47
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-10 22:58:49 +0000

    Update googlesearch.py

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -7,8 +7,8 @@ class GoogleSearch(commands.Cog):
   @commands.command()
   async def choose(self, ctx, *choices):
     """Get a google URL."""
-      search = [escape(s, mass_mentions=True) for s in search if s]
-      if len(search) < 2:
-          await ctx.send(_("Not enough options to pick from."))
-      else:
-          await ctx.send(search)
+    search = [escape(s, mass_mentions=True) for s in search if s]
+    if len(search) < 2:
+        await ctx.send(_("Not enough options to pick from."))
+    else:
+        await ctx.send(search)
