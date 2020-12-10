902fcff1f39ea192458ccd7073b4d87d1b842e92
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-10 22:57:30 +0000

    tests

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -1,1 +1,14 @@
+import discord
+from redbot.core import commands
 
+class GoogleSearch(commands.Cog):
+  """Search google with simplified URLs."""
+  
+  @commands.command()
+  async def choose(self, ctx, *choices):
+    """Get a google URL."""
+      search = [escape(s, mass_mentions=True) for s in search if s]
+      if len(choices) < 2:
+          await ctx.send(_("Not enough options to pick from."))
+      else:
+          await ctx.send(choices)
