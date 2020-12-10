cbc0b2a0ceb9a34d129b0be0052c0f523148dbd0
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-10 23:01:25 +0000

    Update googlesearch.py

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -8,7 +8,7 @@ class GoogleSearch(commands.Cog):
     self.bot = bot
   
   @commands.command()
-  async def search(self, ctx, *choices):
+  async def google(self, ctx, *choices):
     """Get a google URL."""
     search = [escape(s, mass_mentions=True) for s in search if s]
     if len(search) < 2:
