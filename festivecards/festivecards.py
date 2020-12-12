0b7aa7e52882b51e026376f4aa0bf801baca43cb
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-12 16:19:19 +0000

    Final fix

--- festivecards/festivecards.py
+++ festivecards/festivecards.py
@@ -12,19 +12,18 @@ class FestiveCards(commands.Cog):
   async def card(self, ctx: commands.Context, user_id: int, *, message: str):
     """Send a christmas card to someone!"""
     destination = self.bot.get_user(user_id)
-    username = user.name
     author = ctx.author.name
     user = destination
     if destination is None or destination.bot:
         await ctx.send("Invalid ID, user not found, or user is a bot.")
     foot = (f"Send christmas cards using {ctx.clean_prefix}card!")
     e = discord.Embed(title=f":christmas_tree: Christmas Card from {ctx.author} :christmas_tree:", 
-                      description= "Dear {},\n\n{}\n\nFrom {} :gift:".format(username, message, author), 
+                      description= "Dear {},\n\n{}\n\nFrom {} :gift:".format(user.name, message, author), 
                       colour=discord.Colour.red())
     e.set_footer(text=foot)
     try:
       await destination.send(embed=e)
     except discord.HTTPException:
-      await ctx.send("Sorry, I couldn't send a card to {}").format(username)
+      await ctx.send("Sorry, I couldn't send a card to {}").format(user.name)
     else:
-      await ctx.send(f"Message delivered to {username}")
+      await ctx.send(f"Message delivered to {user.name}")
