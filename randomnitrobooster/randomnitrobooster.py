bc46fbad8e693d817fc5fba3e5f4a25750be24e8
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-12 23:30:52 +0000

    Testing

---
+++ randomnitrobooster/randomnitrobooster.py
@@ -0,0 +1,17 @@
+from redbot.core import commands
+
+class RandomNitroBooster(commands.Cog):
+  """Get a random nitro booster."""
+  
+  def __init__(self, bot):
+    self.bot = bot
+    self.config = Config.get_conf(
+        self, identifier=176070082584248320, force_registration=True
+    )
+    self.config.register_guild()
+    
+  @commands.command()
+  async def randomnitro(self, ctx):
+  nitroer = guild.premium_subscribers
+  await ctx.send(f"{random.choice(nitroer{name}))").format(name=.name)
+  
