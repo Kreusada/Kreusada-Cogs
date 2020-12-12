74d7b3051dff97f2e929f0456200624c5cce838b
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-12 12:02:22 +0000

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -67,6 +67,8 @@ class Staff(commands.Cog):
             else ctx.channel
         jumper_link = ctx.message.jump_url
         author_id = ctx.author.id
+        bun = bot.user.name
+        buau = bot.user.avatar_url
         msgtime = f"**Occurance at:** {datetime.now()}"
         authid = f"\n**Author ID:**{author_id}"
         chfmi = "Click here for more information"
@@ -76,8 +78,8 @@ class Staff(commands.Cog):
             embed = Embed.create(
                 self, ctx, title=":warning: ALERT!",
                 description= f"**{ctx.author.name}**{call}{ctx.channel.mention}.{authid}{msgtime}{jumper_f}",
-                footer_text=f"{bot.user.name} | Staff",
-                footer_url=f"{bot.user.avatar_url}",
+                footer_text=f"{bun} | Staff",
+                footer_url=f"{buau}",
             )
             await channel.send(content=f":warning: {role.mention}", allowed_mentions=discord.AllowedMentions(roles=True), embed=embed, delete_after=43200)
         else:
