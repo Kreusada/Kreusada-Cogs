748b68e81d05ee956ddcf753a2d3b2f2b5fe8782
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-12 11:25:50 +0000

    Fixes

--- staff/staff.py
+++ staff/staff.py
@@ -79,7 +79,8 @@ class Staff(commands.Cog):
             embed = Embed.create(
                 self, ctx, title='<:alert:777928824670388254> ALERT!',
                 description= f"**{ctx.author.name}**{call}{ctx.channel.mention}.{authid}{msgtime}{jumper_f}",
-                footer_text=f"{bot.user.name} | Staff"
+                footer_text=f"{bot.user.name} | Staff",
+                footer_url=bot.user.avatar_url
             )
             await channel.send(content=f":warning: {role.mention}", allowed_mentions=discord.AllowedMentions(roles=True), embed=embed, delete_after=43200)
         else:
@@ -107,9 +108,3 @@ class Embed:
             data.set_image(url=image)
         if thumbnail is not None:
             data.set_thumbnail(url=thumbnail)
-        if footer_text is None:
-            footer_text = "Staff"
-        if footer_url is None:
-            footer_url = bot.user.avatar_url
-        data.set_footer(text=footer_text, icon_url=footer_url)
-        return data
