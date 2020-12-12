4917b14e37b38c4959fd598ff85f95ca4f4bb113
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-12 15:13:50 +0000

    Update christmascard.py

--- christmascard/christmascard.py
+++ christmascard/christmascard.py
@@ -1,1 +1,51 @@
+import discord
+from redbot.core import commands
 
+class ChristmasCard(commands.Cog):
+  """Send someone a christmas card!"""
+
+    @commands.command()
+    async def christmascard(self, ctx: commands.Context, user_id: int, *, message: str):
+      """Send a christmas card to someone!"""
+        destination = self.bot.get_user(user_id)
+        if destination is None or destination.bot:
+            await ctx.send(
+                _(
+                    "Invalid ID, user not found, or user is a bot. "
+                    "You can only send messages to people I share "
+                    "a server with."
+                )
+            )
+            return
+
+        prefixes = await ctx.bot.get_valid_prefixes()
+        prefix = re.sub(rf"<@!?{ctx.me.id}>", f"@{ctx.me.name}".replace("\\", r"\\"), prefixes[0])
+        description = _("Owner of {}").format(ctx.bot.user)
+        content = _("Send christmas cards using {}christmascard!").format(prefix)
+        if await ctx.embed_requested():
+            e = discord.Embed(colour=discord.Colour.red(), description=message)
+
+            e.set_footer(text=content)
+            if ctx.bot.user.avatar_url:
+                e.set_author(name=description, icon_url=ctx.bot.user.avatar_url)
+            else:
+                e.set_author(name=description)
+
+            try:
+                await destination.send(embed=e)
+            except discord.HTTPException:
+                await ctx.send(
+                    _("Sorry, I couldn't send a card to {}").format(destination)
+                )
+            else:
+                await ctx.send(_("Message delivered to {}").format(destination))
+        else:
+            response = "{}\nMessage:\n\n{}".format(description, message)
+            try:
+                await destination.send("{}\n{}".format(box(response), content))
+            except discord.HTTPException:
+                await ctx.send(
+                    _("Sorry, I couldn't deliver your message to {}").format(destination)
+                )
+            else:
+                await ctx.send(_("Message delivered to {}").format(destination))
