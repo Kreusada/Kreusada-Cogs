33ac33616932252bc122c329a0f2b22aca320ca8
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-12 15:34:45 +0000

    Changes

--- christmascard/christmascard.py
+++ christmascard/christmascard.py
@@ -13,18 +13,15 @@ class ChristmasCard(commands.Cog):
     destination = self.bot.get_user(user_id)
     if destination is None or destination.bot:
         await ctx.send("Invalid ID, user not found, or user is a bot.")
-    description = ("Christmas Card from {}").format(ctx.author)
     content = ("Send christmas cards using {}card!").format(ctx.clean_prefix)
-    #author = ctx.author.name
-    e = discord.Embed(colour=discord.Colour.red(), description=message)
+    e = discord.Embed(title=f"Christmas Card from {ctx.author}" description=message, colour=discord.Colour.red(), description=message)
     e.set_footer(text=content)
-    #e.set_author(author)
     try:
       await destination.send(embed=e)
     except discord.HTTPException:
       await ctx.send("Sorry, I couldn't send a card to {}").format(destination)
     else:
-        await ctx.send("Message delivered to {}").format(destination)
+      await ctx.send("Message delivered to {}").format(destination)
     response = "{}\nMessage:\n\n{}".format(description, message)
     try:
         await destination.send("{}\n{}".format(box(response), content))
