7838ecaad5a18b570b5252b37ab373897d494c42
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 17:05:57 +0000

    just wanted to see if this would work

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -11,7 +11,7 @@ class GoogleSearch(commands.Cog):
     """Search google with a simple URL."""
     UKquerytemplate = f"https://www.google.com/search?q={search_query}&rlz=1C1CHBF_en-GBGB921GB921&oq=ping&aqs=chrome..69i57j46i433j46i199i291i433j0i433l4j0.1173j0j7&sourceid=chrome&ie=UTF-8"
     chfmi = "Click here for more information"
-    foot = "Please note that this may only work for residents in the UK."
+    footer = "Please note that this may only work for residents in the UK."
     e = discord.Embed(title=f"{ctx.author.name} searched for... {search_query}.",
                       description="**[{}]({})**".format(chfmi, UKquerytemplate),
                       colour=discord.Colour.red())
