7407e8e4b2a6580eb7039d58ddf1f340cc64b708
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 18:06:01 +0000

    [GoogleSearch] Datetime Module as Embed Footer

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -1,4 +1,5 @@
 from redbot.core import commands
+from datetime import datetime
 import discord
 
 class GoogleSearch(commands.Cog):
@@ -12,10 +13,12 @@ class GoogleSearch(commands.Cog):
     querytemplate = f"https://www.google.co.uk/search?source=hp&ei=z07WX6SiGrXVgwfdpa3wAQ&q={search_query.title()}"
     multipleargs = querytemplate.replace(' ', '%20')
     chfmi = "Click here for search results"
-    footer = f"Search google by using: {ctx.clean_prefix}google <search_query>"
+    now = datetime.now()
+    strftime = now.strftime("Today at %H:%M %p")
+    footer = f"{strftime}"
     hassearched = f"{ctx.author.name} searched for... **{search_query.title()}**."
     e = discord.Embed(title="<:google:787733816566349835>  Google Search",
                       description="{}\n\n**[{}]({})**".format(hassearched, chfmi, multipleargs),
                       colour=discord.Colour.red())
-    #e.set_footer(text=footer)
+    e.set_footer(text=footer)
     await ctx.send(embed=e)
