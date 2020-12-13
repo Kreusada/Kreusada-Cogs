b3310f40151842137ff5c4012dfde349cce520f8
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 17:35:42 +0000

    UI changes

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -12,8 +12,9 @@ class GoogleSearch(commands.Cog):
     querytemplate = f"https://www.google.co.uk/search?source=hp&ei=z07WX6SiGrXVgwfdpa3wAQ&q={search_query}"
     chfmi = "Click here for more information"
     footer = f"Search google by using: {ctx.clean_prefix}google <search_query>"
-    e = discord.Embed(title=f"{ctx.author.name} searched for... '{search_query.title()}'.",
-                      description="**[{}]({})**".format(chfmi, querytemplate),
+    hassearched = f"{ctx.author.name} searched for... '{search_query.title()}'."
+    e = discord.Embed(title="<:google:787733816566349835> Google Search",
+                      description="{}\n\n**[{}]({})**".format(hassearched, chfmi, querytemplate),
                       colour=discord.Colour.red())
     e.set_footer(text=footer)
     await ctx.send(embed=e)
