8eeb49076862c5cc5495bf1d577ccabfdd4ea9a5
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 17:54:57 +0000

    fixes for multiple args

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -10,11 +10,12 @@ class GoogleSearch(commands.Cog):
   async def google(self, ctx, *, search_query):
     """Search google with a simple URL."""
     querytemplate = f"https://www.google.co.uk/search?source=hp&ei=z07WX6SiGrXVgwfdpa3wAQ&q={search_query.title()}"
+    multipleargs = "%20".join(querytemplate.title())
     chfmi = "Click here for search results"
     footer = f"Search google by using: {ctx.clean_prefix}google <search_query>"
-    hassearched = f"{ctx.author.name} searched for... **{search_query.title()}**."
+    hassearched = f"{ctx.author.name} searched for... **{multipleargs}**."
     e = discord.Embed(title="<:google:787733816566349835>  Google Search",
-                      description="{}\n\n**[{}]({})**".format(hassearched, chfmi, querytemplate),
+                      description="{}\n\n**[{}]({})**".format(hassearched, chfmi, multipleargs),
                       colour=discord.Colour.red())
     #e.set_footer(text=footer)
     await ctx.send(embed=e)
