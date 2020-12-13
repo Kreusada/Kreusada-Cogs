1db13da71ffb496d37f58e690e7114074c30d958
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 18:08:02 +0000

    [GoogleSearch] Final User Interface Changes

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -16,9 +16,10 @@ class GoogleSearch(commands.Cog):
     now = datetime.now()
     strftime = now.strftime("Today at %H:%M %p")
     footer = f"{strftime}"
-    hassearched = f"{ctx.author.name} searched for... **{search_query.title()}**."
-    e = discord.Embed(title="<:google:787733816566349835>  Google Search",
+    hassearched = f"{ctx.author.name} searched for: **{search_query.title()}**."
+    e = discord.Embed(title=":desktop: Google Search",
                       description="{}\n\n**[{}]({})**".format(hassearched, chfmi, multipleargs),
                       colour=discord.Colour.red())
     e.set_footer(text=footer)
+    e.set_thumbnail("https://media.discordapp.net/attachments/769165401879478302/787742449987878972/google_icon_131222.png")
     await ctx.send(embed=e)
