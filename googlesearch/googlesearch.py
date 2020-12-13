1c418ed6adaf8d2aecdb00d494a62b9b0f8b1780
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 17:22:36 +0000

    Simplified URL with joins

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -9,11 +9,12 @@ class GoogleSearch(commands.Cog):
   @commands.command()
   async def google(self, ctx, search_query: str):
     """Search google with a simple URL."""
-    UKquerytemplate = f"https://www.google.com/search?q={search_query}&rlz=1C1CHBF_en-GBGB921GB921&oq=ping&aqs=chrome..69i57j46i433j46i199i291i433j0i433l4j0.1173j0j7&sourceid=chrome&ie=UTF-8"
+    joinwords = "%".join(search_query)
+    querytemplate = f"https://www.google.com/search?q={joinwords}"
     chfmi = "Click here for more information"
-    footer = "Please note that this may only work for residents in the UK."
+    footer = "Search google by using: {ctx.clean_prefix}google <search_query>"
     e = discord.Embed(title=f"{ctx.author.name} searched for... {search_query}.",
-                      description="**[{}]({})**".format(chfmi, UKquerytemplate),
+                      description="**[{}]({})**".format(chfmi, querytemplate),
                       colour=discord.Colour.red())
     e.set_footer(text=footer)
     await ctx.send(embed=e)
