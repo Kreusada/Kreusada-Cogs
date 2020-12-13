106bba6874eef65879401661f7eb202e34d2ed26
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 17:29:24 +0000

    Better URL this time

--- googlesearch/googlesearch.py
+++ googlesearch/googlesearch.py
@@ -9,11 +9,10 @@ class GoogleSearch(commands.Cog):
   @commands.command()
   async def google(self, ctx, search_query: str):
     """Search google with a simple URL."""
-    joinwords = "%".join(search_query)
-    querytemplate = f"https://www.google.com/search?q={joinwords}"
+    querytemplate = f"https://www.google.co.uk/search?source=hp&ei=z07WX6SiGrXVgwfdpa3wAQ&q={search_query}"
     chfmi = "Click here for more information"
     footer = f"Search google by using: {ctx.clean_prefix}google <search_query>"
-    e = discord.Embed(title=f"{ctx.author.name} searched for... {search_query}.",
+    e = discord.Embed(title=f"{ctx.author.name} searched for... '{search_query.title()}'.",
                       description="**[{}]({})**".format(chfmi, querytemplate),
                       colour=discord.Colour.red())
     e.set_footer(text=footer)
