418a3691bde13ccffbad78a7818a262127631026
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-13 18:26:02 +0000

    [AppendBackslash] Removed command.Context from functions

--- appendbackslash/abs.py
+++ appendbackslash/abs.py
@@ -9,7 +9,7 @@ class AppendBackslash(commands.Cog):
     self.bot = bot
     
   @commands.command(name="abs", aliases=["backslash", "backslashemoji"])
-  async def appendbackslash(self, ctx: commands.Context, object_to_append: str):
+  async def appendbackslash(self, ctx, object_to_append: str):
     """Append a backslash to an object."""
     ota = object_to_append
     await ctx.send(''.join(('\\',ota)))
