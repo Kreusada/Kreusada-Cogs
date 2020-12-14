e3f40fa71f1c1689ee8ccd92e68b041f00b8a34c
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-14 16:33:39 +0000

    [FancyUptime] Removed unnecessary content

--- fancyuptime/delta_utils.py
+++ fancyuptime/delta_utils.py
@@ -5,26 +5,6 @@ from typing import Optional, SupportsInt
 def humanize_timedelta(
     *, timedelta: Optional[datetime.timedelta] = None, seconds: Optional[SupportsInt] = None
 ) -> str:
-    """
-    Get a locale aware human timedelta representation.
-    This works with either a timedelta object or a number of seconds.
-    Fractional values will be omitted, and values less than 1 second
-    an empty string.
-    Parameters
-    ----------
-    timedelta: Optional[datetime.timedelta]
-        A timedelta object
-    seconds: Optional[SupportsInt]
-        A number of seconds
-    Returns
-    -------
-    str
-        A locale aware representation of the timedelta or seconds.
-    Raises
-    ------
-    ValueError
-        The function was called with neither a number of seconds nor a timedelta object
-    """
 
     try:
         obj = seconds if seconds is not None else timedelta.total_seconds()
