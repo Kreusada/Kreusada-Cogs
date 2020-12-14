56ef1042e5e7315a985883e7ee2f280ad0d471a1
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-14 20:37:37 +0000

    [AntiBotRole] Cog Creation

---
+++ antibotrole/antibotrole.py
@@ -0,0 +1,24 @@
+import discord
+from redbot.core import commands
+
+class AntiBotRole(commands.Cog):
+  """Invite a bot to your server without their automated role."""
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command(aliases=["botinv"])
+  async def invbot(self, ctx, bot_id: int):
+    user = self.bot.get_user(bot_id)
+    if user.bot is True:
+      inv = f"https://discord.com/oauth2/authorize?client_id={bot_id}&scope=bot"
+      chti = "Click here to invite"
+      e = discord.Embed(title=f"{user.name} Invite Link (Without Role)",
+                        description="**ID: {}**\n\n**[{}]({})**".format(user.id, chti, inv),
+                        color=x0b7fff3)
+      e.set_author(name=bot.user.name, icon=bot.user.avatar_url)                  
+      await ctx.send(embed=e)
+    else:
+      await ctx.send(f"Please enter a valid bot ID. **{bot_id} was not recognised.**")
+    
+def setup(bot):
+  bot.add_cog(AntiBotRole(bot))
