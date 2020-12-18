91e0aafab6ab6f2b0e2fcb8b4881d0109c10dfd4
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-18 18:54:08 +0000

    [AntiBotRole] Error Proning for IDs that don't match user.bot

--- antibotrole/antibotrole.py
+++ antibotrole/antibotrole.py
@@ -9,17 +9,20 @@ class AntiBotRole(commands.Cog):
   @commands.command(aliases=["botinv"])
   async def invbot(self, ctx, bot_id: int):
     user = self.bot.get_user(bot_id)
-    if user.bot is True:
-      bot = self.bot
-      inv = f"https://discord.com/oauth2/authorize?client_id={bot_id}&scope=bot"
-      chti = "Click here to invite"
-      e = discord.Embed(title=f"{user.name} Invite Link (Without Role)",
-                        description="**ID: {}**\n\n**[{}]({})**".format(user.id, chti, inv),
-                        color=0xb7fff3)
-      e.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)                  
-      await ctx.send(embed=e)
-    else:
-      await ctx.send(f"Please enter a valid bot ID.\n**{bot_id} was not recognised.**")
+    try:
+      if user.bot is True:
+        bot = self.bot
+        inv = f"https://discord.com/oauth2/authorize?client_id={bot_id}&scope=bot"
+        chti = "Click here to invite"
+        e = discord.Embed(title=f"{user.name} Invite Link (Without Role)",
+                          description="**ID: {}**\n\n**[{}]({})**".format(user.id, chti, inv),
+                          color=0xb7fff3)
+        e.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)                  
+        await ctx.send(embed=e)
+      else:
+        await ctx.send(f"Please enter a valid bot ID.\n**{bot_id} was not recognised.**")
+    except:
+      await ctx.send(f"Something went wrong. Make sure that the ID is a Discord bot's.")
     
 def setup(bot):
   bot.add_cog(AntiBotRole(bot))
