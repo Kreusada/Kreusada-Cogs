0b86606c5b17bf0744c8322d30acebca1e179279
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-19 15:10:21 +0000

    [FancyUptime] Reviewing False Inlines for Embed Fields

--- fancyuptime/fancyuptime.py
+++ fancyuptime/fancyuptime.py
@@ -38,11 +38,11 @@ class FancyUptime(commands.Cog):
                           f"Therefore, it's been online for **{uptime_str}**."
                         ),
                         color=0x59e1ac)
-      e.add_field(name="Instance name:", value=bot.name, inline=True)
-      e.add_field(name="Current guild:", value=guild, inline=True)
-      e.add_field(name="Number of servers:", value=servers, inline=True)
-      e.add_field(name="Unique users:", value=users, inline=True)
-      e.add_field(name="Commands available:", value=commandsavail, inline=True)
+      e.add_field(name="Instance name:", value=bot.name, inline=False)
+      e.add_field(name="Current guild:", value=guild, inline=False)
+      e.add_field(name="Number of guilds:", value=servers, inline=False)
+      e.add_field(name="Unique users:", value=users, inline=False)
+      e.add_field(name="Commands available:", value=commandsavail, inline=False)
       e.set_footer(text=f"{strftime}")
       await ctx.send(embed=e)
 
