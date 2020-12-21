2e3a3ee3056d51918e5230dff365becf3db711ca
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-21 15:23:41 +0000

    [Shrug] ¯\_(ツ)_/¯

---
+++ shrug/shrug.py
@@ -0,0 +1,13 @@
+import discord
+from redbot.core import commands
+
+class Shrug(commands.Cog):
+  """¯\_(ツ)_/¯"""
+  
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command()
+  async def shrug(self, ctx, message: str):
+    """¯\_(ツ)_/¯"""
+    await ctx.send(''.join(('¯\_(ツ)_/¯',message)))
