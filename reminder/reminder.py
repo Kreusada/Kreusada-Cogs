17a44c262c4245e0f81101a1df236c7180980497
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-21 15:44:54 +0000

    [Reminder] Logical Timeframes

--- reminder/reminder.py
+++ reminder/reminder.py
@@ -17,27 +17,41 @@ class Reminder(commands.Cog):
     pass
 
   @remind.command()
-  async def me(self, ctx, seconds: int, *, message: str):
+  async def me(self, ctx, hours: int, *, message: str):
     """Remind yourself for later!"""
+    if hours < 1:
+      hours *= 60
+      timeframe = "minutes"
+    else:
+      hours = hours
+      timeframe = "hours"
     reminder = f"{box(message, lang='cmd')}"
-    await ctx.send(f"Okay {ctx.author.name}, I will remind you this in **{seconds} seconds**!")
-    await asyncio.sleep(seconds)
+    await ctx.send(f"Okay {ctx.author.name}, I will remind you this in **{int(hours)} {timeframe}**!")
+    time = hours * 3600
+    await asyncio.sleep(time)
     embed = discord.Embed(title=f"{CLOCK} Reminder!", description=f"{ctx.author.name}, you asked me [here]({ctx.message.jump_url}) to remind you about: {reminder}", color=0xff5151)
-    embed.set_footer(text=f"⏰ You asked me this {seconds} seconds ago.")
+    embed.set_footer(text=f"⏰ You asked me this {int(hours)} {timeframe} ago.")
     try: 
       await ctx.author.send(embed=embed)
     except discord.Forbidden: 
       await ctx.send(content=f"I don't have permissions to send DMs to you, so I'll send here instead, {ctx.author.mention}.", embed=embed)
 
   @remind.command()
-  async def user(self, ctx, user_id: int, seconds: int, *, message: str):
+  async def user(self, ctx, user_id: int, hours: int, *, message: str):
     """Remind a Discord user."""
+    if hours < 1:
+      hours *= 60
+      timeframe = "minutes"
+    else:
+      hours = hours
+      timeframe = "hours"
     reminder = f"{box(message, lang='cmd')}"
     user = self.bot.get_user(user_id)
-    await ctx.send(f"Okay {ctx.author.name}, I will remind {user.name} about this in **{seconds} seconds**!")
-    await asyncio.sleep(seconds)
+    await ctx.send(f"Okay {ctx.author.name}, I will remind {user.name} about this in **{int(hours)} {timeframe}**!")
+    time = hours * 3600
+    await asyncio.sleep(time)
     embed = discord.Embed(title=f"{CLOCK} Reminder!", description=f"{ctx.author.name}, wanted to remind you [here]({ctx.message.jump_url}) about: {reminder}", color=0xff5151)
-    embed.set_footer(text=f"{CLOCK} {ctx.author.name} requested this {seconds} seconds ago.")
+    embed.set_footer(text=f"{CLOCK} {ctx.author} requested this {int(hours)} {timeframe} ago.")
     try: 
       await user.send(embed=embed)
     except discord.Forbidden: 
