7ede15b268be6913a5c5caba490f8d40bb603a23
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-01 23:52:07 +0000

    [RomanConverter] Cog is Ready

--- romanconverter/romanconverter.py
+++ romanconverter/romanconverter.py
@@ -30,7 +30,12 @@ class RomanConverter(commands.Cog):
         return output
 
     async def number(self, roman_numeral):
-        rom_val = {'I': 1, 'i': 1, 'V': 5, 'v': 5, 'X': 10, 'x': 10, 'L': 50, 'l': 50, 'C': 100, 'c': 100, 'D': 500, 'd': 500, 'M': 1000, 'm': 1000}
+        rom_val = {
+            'I': 1, 'i': 1, 'V': 5, 'v': 5, 
+            'X': 10, 'x': 10, 'L': 50, 'l': 50, 
+            'C': 100, 'c': 100, 'D': 500, 'd': 500, 
+            'M': 1000, 'm': 1000
+        }
         output = 0
         rn = roman_numeral
         for i in range(len(rn)):
