aab5b6a67e307755c3f4bcf3cc910624bf610ac2
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-15 14:54:50 +0000

    [SearchEngine] Vast UI Improvements

--- searchengine/searchengine.py
+++ searchengine/searchengine.py
@@ -24,51 +24,51 @@ class SearchEngine(commands.Cog):
   async def google(self, ctx, *, search_query):
     """Search google."""
     querytemplate = f"https://www.google.co.uk/search?source=hp&ei=z07WX6SiGrXVgwfdpa3wAQ&q={search_query.capitalize()}"
-    multipleargs = querytemplate.replace(' ', '%20')
-    chfmi = "Click here for search results"
-    now = datetime.now()
-    strftime = now.strftime("Today at %H:%M %p")
-    footer = f"{strftime}"
+    querytemplate = querytemplate.replace(' ', '%20')
+    footer = ctx.message.created_at
     hassearched = f"{ctx.author.name} searched for: **{search_query}**."
-    e = discord.Embed(title="Google Search",
-                      description="{}\n\n**[{}]({})**".format(hassearched, chfmi, multipleargs),
-                      colour=discord.Colour.red())
+    e = discord.Embed(description=(
+      f"Engine: Google\n"
+      f"Search Query: {search_query}\n"
+      f"Author: {ctx.author.name}\n"
+      f"Search results: {querytemplate}"
+    ),
+    colour=discord.Colour.red())
     e.set_footer(text=footer)
-    e.set_thumbnail(url="https://media.discordapp.net/attachments/769165401879478302/787742449987878972/google_icon_131222.png")
     await ctx.send(embed=e)
 
   @commands.command()
   async def pinterest(self, ctx, *, search_query):
     """Search pinterest."""
     querytemplate = f"https://www.pinterest.co.uk/search/pins/?q={search_query.capitalize()}"
-    multipleargs = querytemplate.replace(' ', '%20')
-    chfmi = "Click here for search results"
-    now = datetime.now()
-    strftime = now.strftime("Today at %H:%M %p")
-    footer = f"{strftime}"
+    querytemplate = querytemplate.replace(' ', '%20')
+    footer = ctx.message.created_at
     hassearched = f"{ctx.author.name} searched for: **{search_query}**."
-    e = discord.Embed(title="Pinterest Search",
-                      description="{}\n\n**[{}]({})**".format(hassearched, chfmi, multipleargs),
-                      colour=discord.Colour.red())
+    e = discord.Embed(description=(
+      f"Engine: Pinterest\n"
+      f"Search Query: {search_query}\n"
+      f"Author: {ctx.author.name}\n"
+      f"Search results: {querytemplate}"
+    ),
+    colour=discord.Colour.red())
     e.set_footer(text=footer)
-    e.set_thumbnail(url="https://media.discordapp.net/attachments/769165401879478302/787754393873154069/social_pinterest_icon_131227.png")
     await ctx.send(embed=e)
 
   @commands.command()
   async def redbubble(self, ctx, *, search_query):
     """Search redbubble."""
     querytemplate = f"https://www.redbubble.com/shop/?query={search_query.capitalize()}"
-    multipleargs = querytemplate.replace(' ', '%20')
-    chfmi = "Click here for search results"
-    now = datetime.now()
-    strftime = now.strftime("Today at %H:%M %p")
-    footer = f"{strftime}"
+    querytemplate = querytemplate.replace(' ', '%20')
+    footer = ctx.message.created_at
     hassearched = f"{ctx.author.name} searched for: **{search_query}**."
-    e = discord.Embed(title="Redbubble Search",
-                      description="{}\n\n**[{}]({})**".format(hassearched, chfmi, multipleargs),
-                      colour=discord.Colour.red())
+    e = discord.Embed(description=(
+      f"Engine: RedBubble\n"
+      f"Search Query: {search_query}\n"
+      f"Author: {ctx.author.name}\n"
+      f"Search results: {querytemplate}"
+    ),
+    colour=discord.Colour.red())
     e.set_footer(text=footer)
-    e.set_thumbnail(url="https://user-images.githubusercontent.com/29949873/32622388-4e7a9960-c548-11e7-9be6-adbaca0545f5.png")
     await ctx.send(embed=e)
 
 
