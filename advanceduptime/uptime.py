98b7bfd840d7b07f8b2f2c35a6af7f2df8369d53
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-15 10:56:11 +0000

    [AdvancedUptime] Removed repetition in output

--- advanceduptime/uptime.py
+++ advanceduptime/uptime.py
@@ -34,24 +34,18 @@ class AdvancedUptime(commands.Cog):
   @commands.command()
   async def uptime(self, ctx: commands.Context):
       """Shows [botname]'s uptime."""
-      since = ctx.bot.uptime.strftime("%d/%m/%Y - %H:%M")
       delta = datetime.utcnow() - self.bot.uptime
       uptime_str = humanize_timedelta(timedelta=delta) or ("Less than one second")
-      bot = ctx.bot.user
       botname = ctx.bot.user.name
-      guild = ctx.guild
       users = len(self.bot.users)
       servers = str(len(self.bot.guilds))
       commandsavail = len(set(self.bot.walk_commands()))
       now = datetime.now()
       strftime = now.strftime("Today at %H:%M %p")
       app_info = await self.bot.application_info()
-      if app_info.team:
-        owner = app_info.team.name
-      else:
-        owner = app_info.owner
+      if app_info.team: owner = app_info.team.name
+      else: owner = app_info.owner
       e = discord.Embed(title=f":green_circle:  {botname}'s Uptime", color=0x59e1ac)
-      e.add_field(name=f"{botname} has been up since...", value=since, inline=False)
       e.add_field(name=f"Therefore, {botname} has been up for...", value=uptime_str, inline=False)
       e.add_field(name="Instance name:", value=bot.name, inline=True)
       e.add_field(name="Instance owner:", value=owner, inline=True)
@@ -64,9 +58,9 @@ class AdvancedUptime(commands.Cog):
       await ctx.send(embed=e)
 
 async def setup(bot):
-  fu = AdvancedUptime(bot)
+  au = AdvancedUptime(bot)
   global _old_uptime
   _old_uptime = bot.get_command("uptime")
   if _old_uptime:
       bot.remove_command(_old_uptime.name)
-  bot.add_cog(fu)
+  bot.add_cog(au)
