ba5dfd02b3d0443b2fcf0237c5e73af4b83813fc
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-22 18:09:51 +0000

    [PublishCogs] Setup

--- publishcogs/__init__.py
+++ publishcogs/__init__.py
@@ -1,3 +1,6 @@
-from .publishcogs import setup
+from .publishcogs import PublishCogs
+
+def setup(bot):
+  bot.add_cog(PublishCogs(bot))
 
 __red_end_user_data_statement__ = "This cog does not persistently store data about users."
