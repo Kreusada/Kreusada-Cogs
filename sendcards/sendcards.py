bba9721a2b8fa6248b6d10be69b1aa37e503dac3
Kreusada <kreusadaprojects@gmail.com>
2021-01-30 09:04:09 +0000

    [SendCards] Cog rewrite: it needed it!

--- sendcards/sendcards.py
+++ sendcards/sendcards.py
@@ -26,99 +26,52 @@ class SendCards(commands.Cog):
         Nothing to delete
         """
         return
-
-    @commands.group()
-    async def card(self, ctx: commands.Context):
-        """General settings for cards."""
         
-    @card.group()
+    @commands.group()
     async def send(self, ctx: commands.Context):
         """Send a card to someone!"""
-      
-  # This cog was made a while back, when I just started learning to code.
-  # The original concept of getting a users DMs was taken from 
       
     @send.command()
     async def christmas(self, ctx: commands.Context, user_id: int, *, message: str):
         """Send a christmas card to someone!"""
-        destination = self.bot.get_user(user_id)
-        author = ctx.author.name
-        user = destination
-        if destination is None or destination.bot:
-            await ctx.send("Invalid ID, user not found, or user is a bot.")
-        else:
-          foot = (f"Send christmas cards using: {ctx.clean_prefix}card send christmas!")
-          e = discord.Embed(title=f":christmas_tree: Christmas Card from {ctx.author} :christmas_tree:", 
-                            description= "Dear {},\n\n{}\n\nFrom {} :gift:".format(user.name, message, author), 
-                            colour=discord.Colour.red())
-          e.set_footer(text=foot)
-        try:
-          await destination.send(embed=e)
-        except discord.HTTPException:
-          await ctx.send(f"Sorry, I couldn't send a card to **{user.name}.**")
-        else:
-          await ctx.send(f"Christmas card delivered to **{user.name}!** :gift:")
+        await self.card_send(ctx, 'christmas', user_id, message)
 
     @send.command()
     async def birthday(self, ctx: commands.Context, user_id: int, *, message: str):
         """Send a birthday card to someone!"""
-        destination = self.bot.get_user(user_id)
-        author = ctx.author.name
-        user = destination
-        if destination is None or destination.bot:
-            await ctx.send("Invalid ID, user not found, or user is a bot.")
-        else:
-          foot = (f"Send birthday cards using: {ctx.clean_prefix}card send birthday!")
-          e = discord.Embed(title=f":tada: Birthday Card from {ctx.author} :tada:", 
-                          description= "Dear {},\n\n{}\n\nFrom {} :balloon:".format(user.name, message, author), 
-                          colour=discord.Colour.red())
-          e.set_footer(text=foot)
-        try:
-          await destination.send(embed=e)
-        except discord.HTTPException:
-          await ctx.send(f"Sorry, I couldn't send a card to **{user.name}.**")
-        else:
-          await ctx.send(f"Birthday card delivered to **{user.name}!** :tada:")
+        await self.card_send(ctx, 'birthday', user_id, message)
 
     @send.command(aliases=["gws"])
     async def getwellsoon(self, ctx: commands.Context, user_id: int, *, message: str):
       """Send a get well soon card to someone!"""
-      destination = self.bot.get_user(user_id)
-      author = ctx.author.name
-      user = destination
-      if destination is None or destination.bot:
-          await ctx.send("Invalid ID, user not found, or user is a bot.")
-      else:
-        foot = (f"Send get well soon cards using: {ctx.clean_prefix}card send getwellsoon!")
-        e = discord.Embed(title=f":thermometer_face: Get Well Soon Card from {ctx.author} :thermometer_face:", 
-                        description= "Dear {},\n\n{}\n\nFrom {} :pray:".format(user.name, message, author), 
-                          colour=discord.Colour.red())
-        e.set_footer(text=foot)
-      try:
-        await destination.send(embed=e)
-      except discord.HTTPException:
-        await ctx.send(f"Sorry, I couldn't send a card to **{user.name}.**")
-      else:
-        await ctx.send(f"Get well soon card delivered to **{user.name}!** I hope they're okay too! :pray:")
+      await self.card_send(ctx, 'get well soon', user_id, message)
 
     @send.command()
     async def valentine(self, ctx: commands.Context, user_id: int, *, message: str):
         """Send a valentines card to someone!"""
-        destination = self.bot.get_user(user_id)
-        author = ctx.author.name
-        user = destination
-        if destination is None or destination.bot:
-            await ctx.send("Invalid ID, user not found, or user is a bot.")
+        await self.card_send(ctx, 'valentine', user_id, message)
+
+    async def card_send(self, ctx: commands.Context, type: str, user_id: int, message: str):
+        if len(message) > 1900:
+            return await ctx.send("This message is *too* long. Please try again.")
+        if type == 'christmas':
+            emoji = "\N{CHRISTMAS TREE}"
+        elif type 'birthday':
+            emoji = "\N{PARTY POPPER}"
+        elif type == 'get well soon':
+            emoji = "\N{THERMOMETER}\N{VARIATION SELECTOR-16}"
         else:
-          foot = (f"Send valentine cards using: {ctx.clean_prefix}card send valentine!")
-          romance = ":smiling_face_with_3_hearts:"
-          e = discord.Embed(title=f"{romance} Valentines Card from {ctx.author} {romance}", 
-                          description= "Dear {},\n\n{}\n\nWith love from {} {}".format(user.name, message, author, romance), 
-                            colour=discord.Colour.red())
-          e.set_footer(text=foot)
+            emoji = "\N{SMILING FACE WITH SMILING EYES AND THREE HEARTS}"
+        name = self.bot.get_user(user_id)
+        embed = discord.Embed(
+          title=f"{emoji} {type.title()} card from {ctx.author.name}!",
+          description="Dear {name},\n\n{message}\n\nFrom {ctx.author.name} {emoji}.",
+          color=await ctx.embed_colour()
+        )
+        embed.set_footer(text=f"Send {type} cards by using: {ctx.clean_prefix}send {type.replace(' ', '')}!")
         try:
-          await destination.send(embed=e)
-        except discord.HTTPException:
-          await ctx.send(f"Sorry, I couldn't send a card to **{user.name}.**")
-        else:
-          await ctx.send(f"Valentines card delivered to **{user.name}!** Hey {author}, stop blushing. :wink:")
+            await name.send(embed=embed)
+            return await ctx.send(f"A {type.capitalize()} card has been successfully sent to {name.name}! {emoji}")
+        except discord.Forbidden:
+            return await ctx.send(f"Unfortunately, {name.name} has their DMs turned off. Sorry!")
+
