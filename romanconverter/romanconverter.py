654b4927f2ce4981a9b9186564cccadd2b165da8
Kreusada <kreusadaprojects@gmail.com>
2021-01-30 10:48:00 +0000

    [RomanConverter] Function restructure and attributes

--- romanconverter/romanconverter.py
+++ romanconverter/romanconverter.py
@@ -4,6 +4,41 @@ from redbot.core.i18n import Translator, cog_i18n
 
 _ = Translator("RomanConverter", __file__)
 
+# This function's base was taken and modified from https://www.w3resource.com/python-exercises/class-exercises/python-class-exercise-1.php.
+
+def roman(number):
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV","I"]
+    output = ''
+    i = 0
+    while number > 0:
+        for _ in range(number // val[i]):
+            output += syb[i]
+            number -= val[i]
+        i += 1
+    return output
+
+# This function's base was taken and modified from https://www.w3resource.com/python-exercises/class-exercises/python-class-exercise-2.php.
+
+def number(roman_numeral):
+    """Roman number to number."""
+    rom_val = {
+        'I': 1, 'i': 1, 'V': 5, 'v': 5, 'X': 10, 'x': 10, 'L': 50, 'l': 50, 
+        'C': 100, 'c': 100, 'D': 500, 'd': 500, 'M': 1000, 'm': 1000
+    }
+    output = 0
+    rn = roman_numeral
+    for i in range(len(rn)):
+        try:
+            if i > 0 and rom_val[rn[i]] > rom_val[rn[i - 1]]:
+                output += rom_val[rn[i]] - 2 * rom_val[rn[i - 1]]
+            else:
+                output += rom_val[rn[i]]
+        except KeyError:
+            msg = "That doesn't look like roman numerals."
+            return msg
+    return output
+
 @cog_i18n(_)
 class RomanConverter(commands.Cog):
     """Convert integers to roman numerals."""
@@ -23,50 +58,6 @@ class RomanConverter(commands.Cog):
         Nothing to delete
         """
         return
-
-    async def roman(self, number):
-        """Number to roman numeral"""
-        val = [
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4,
-            1
-            ]
-        syb = [
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV",
-            "I"
-            ]
-        output = ''
-        i = 0
-        while number > 0:
-            for _ in range(number // val[i]):
-                output += syb[i]
-                number -= val[i]
-            i += 1
-        return output
-
-    async def number(self, roman_numeral):
-        """Roman number to number."""
-        rom_val = {
-            'I': 1, 'i': 1, 'V': 5, 'v': 5, 
-            'X': 10, 'x': 10, 'L': 50, 'l': 50, 
-            'C': 100, 'c': 100, 'D': 500, 'd': 500, 
-            'M': 1000, 'm': 1000
-        }
-        output = 0
-        rn = roman_numeral
-        for i in range(len(rn)):
-            try:
-                if i > 0 and rom_val[rn[i]] > rom_val[rn[i - 1]]:
-                    output += rom_val[rn[i]] - 2 * rom_val[rn[i - 1]]
-                else:
-                    output += rom_val[rn[i]]
-            except KeyError:
-                msg = "That doesn't look like roman numerals."
-                return msg
-        return output
 
     @commands.command()
     async def romanize(self, ctx: commands.Context, number: int):
