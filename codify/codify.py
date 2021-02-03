7f696ad9d40e5c70880b894474bea37882a19ea8
kreusada <67752638+kreus7@users.noreply.github.com>
2021-02-03 10:08:56 +0000

    [Codify] Feature: Escape markdown

--- codify/codify.py
+++ codify/codify.py
@@ -1,6 +1,6 @@
 import discord
 from redbot.core import commands
-from redbot.core.utils.chat_formatting import box
+from redbot.core.utils.chat_formatting import box, escape
 
 class Codify(commands.Cog):
     """
@@ -22,7 +22,7 @@ class Codify(commands.Cog):
         return
 
     @commands.command()
-    async def codify(self, ctx: commands.Context, message_id: int, language: str = "python"): 
+    async def codify(self, ctx: commands.Context, message_id: int, language: str = "python", escape_markdown: bool = False): 
         """
         Get a message and wrap it in a codeblock.
         
@@ -32,6 +32,7 @@ class Codify(commands.Cog):
         """
         try:
             message = await ctx.fetch_message(message_id)
-            await ctx.send(box(message.content, lang=language))
+            send = box(message.content, lang=language))
+            await ctx.send(escape(send)) if escape_markdown else await ctx.send(send)
         except discord.NotFound:
             await ctx.send(f"Could not find a message with the ID: `{message_id}`.")
