23168667423157294a279bceb00780b6213f5133
Kreusada <kreusadaprojects@gmail.com>
2021-02-10 21:34:42 +0000

    [RomanConverter] Logistic Updates

--- romanconverter/__init__.py
+++ romanconverter/__init__.py
@@ -1,4 +1,10 @@
+import json
+from pathlib import Path
+
 from .roman import RomanConverter
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 def setup(bot):
     bot.add_cog(RomanConverter(bot))
