3447418f2bb58b032e5602bf262b7a7e04d8e4a9
Kreusada <kreusadaprojects@gmail.com>
2021-03-06 12:43:22 +0000

    run -l99 black

--- vinfo/vinfo.py
+++ vinfo/vinfo.py
@@ -13,8 +13,11 @@ log = logging.getLogger("red.kreusada.vinfo")
 
 base = "{}: {}\n{}: {}.{}.{}\n{}: {}\n\n{}: {}\n{}: {}"
 
-RETURN_TYPE_1 = box("Could not find a version for `{}`. If this is a builtin module, if will follow your python version ({}.{}.{}).", lang='py')
-RETURN_TYPE_2 = box("- You do not have an installed module named `{}`.", lang='diff')
+RETURN_TYPE_1 = box(
+    "Could not find a version for `{}`. If this is a builtin module, if will follow your python version ({}.{}.{}).",
+    lang="py",
+)
+RETURN_TYPE_2 = box("- You do not have an installed module named `{}`.", lang="diff")
 RETURN_TYPE_3 = "Builtin Red cogs do not have version attributes by default. Perhaps you're looking for your Red version, which would be {}."
 
 REDBOT_CORE_COGS = [
@@ -45,7 +48,9 @@ class Vinfo(commands.Cog):
     Get versions of 3rd party cogs, and modules.
     """
 
-    __author__ = ["Kreusada", ]
+    __author__ = [
+        "Kreusada",
+    ]
     __version__ = "1.0.1"
 
     def __init__(self, bot):
@@ -79,7 +84,7 @@ class Vinfo(commands.Cog):
         return f"{context}\n\nAuthor: {authors}\nVersion: {self.__version__}"
 
     # Commands
-    
+
     @commands.is_owner()
     @commands.group(aliases=["versioninfo"])
     async def vinfo(self, ctx):
@@ -95,13 +100,13 @@ class Vinfo(commands.Cog):
 
         if cog not in self.bot.cogs:
             return await ctx.send(f"Could not find a cog matching `{cog}`.")
-        
+
         Cog = self.bot.get_cog(cog)
-        
+
         # Note that cogs won't have a `version_info` attr unlike some modules, so
         # we'll skip finding that attr because it will return False 99% of the time.
 
-        if hasattr(Cog, '__version__'):
+        if hasattr(Cog, "__version__"):
             return await ctx.send(f"{cog} version: `{getattr(Cog, '__version__')}`.")
         elif cog in REDBOT_CORE_COGS:
             return await ctx.send(RETURN_TYPE_3.format(redbot.version_info))
@@ -115,14 +120,16 @@ class Vinfo(commands.Cog):
         if not module:
             embed = self.modvinfo_format(base)
             embed.color = await ctx.embed_colour()
-            embed.set_footer(text="Find a specific module version by adding the module argument.")
+            embed.set_footer(
+                text="Find a specific module version by adding the module argument."
+            )
             await ctx.send(embed=embed)
             return await ctx.send_help()
 
         # If `version_info` is defined, we should refer to this first.
-        version_info = 'version_info'
-        versionattr = '__version__'
-        
+        version_info = "version_info"
+        versionattr = "__version__"
+
         pypath = str(distutils.sysconfig.get_python_lib(standard_lib=True))
 
         try:
@@ -131,14 +138,16 @@ class Vinfo(commands.Cog):
             return await ctx.send(RETURN_TYPE_2.format(module))
 
         if hasattr(MOD, version_info):
-            vinfo = [getattr(MOD, version_info), '.' + version_info]
+            vinfo = [getattr(MOD, version_info), "." + version_info]
         elif hasattr(MOD, versionattr):
-            vinfo = [getattr(MOD, versionattr), '.' + version_info]
+            vinfo = [getattr(MOD, versionattr), "." + version_info]
         elif MOD.__file__.lower().startswith(pypath.lower()):
-                vinfo = [(sys.version_info[:3]), " [Python Builtin]"]
+            vinfo = [(sys.version_info[:3]), " [Python Builtin]"]
         else:
             log.info(f"[From {ctx.channel.id}] {module} path: {MOD.__file__}")
-            return await ctx.send(RETURN_TYPE_1.format(MOD.__name__, *sys.version_info[:3]))
+            return await ctx.send(
+                RETURN_TYPE_1.format(MOD.__name__, *sys.version_info[:3])
+            )
 
         if isinstance(vinfo[0], tuple):
             value = "{}.{}.{}".format(*vinfo[0])
@@ -149,6 +158,7 @@ class Vinfo(commands.Cog):
 
         await ctx.send(
             box(
-                f'Attribute: `{MOD.__name__}{vinfo[1]}`\nFound version info for [{module}]: {value}', lang='yaml'
+                f"Attribute: `{MOD.__name__}{vinfo[1]}`\nFound version info for [{module}]: {value}",
+                lang="yaml",
             )
         )
