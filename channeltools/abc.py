1d59bb0c457d76034078872b861a182350868101
Kreusada <kreusadaprojects@gmail.com>
2021-03-21 18:47:06 +0000

    Add Config to abc class

--- channeltools/abc.py
+++ channeltools/abc.py
@@ -1,7 +1,9 @@
 import abc
 
+from redbot.core import Config
 from redbot.core.bot import Red
 
 class MixinMeta(abc.ABC):
     def __init__(self, *args):
         self.bot: Red
+        self.config: Config
