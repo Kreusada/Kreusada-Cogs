910a48ddfb6187c9c82ed8ed995c66f2321486cf
Kreusada <kreusadaprojects@gmail.com>
2021-03-23 21:12:28 +0000

    Initial commit

---
+++ alphanato/README.rst
@@ -0,0 +1,69 @@
+.. _alphanato:
+
+=========
+AlphaNato
+=========
+
+This is the cog guide for the alphanato cog. You will
+find detailed docs about usage and commands.
+
+``[p]`` is considered as your prefix.
+
+.. note:: To use this cog, load it by typing this::
+
+        [p]load alphanato
+
+.. _alphanato-usage:
+
+-----
+Usage
+-----
+
+Get the nato alphabet name from a letter.
+
+
+.. _alphanato-commands:
+
+--------
+Commands
+--------
+
+.. _alphanato-command-nato:
+
+^^^^
+nato
+^^^^
+
+**Syntax**
+
+.. code-block:: none
+
+    [p]nato <letters...>
+
+**Description**
+
+Get the nato alphabet name from a letter.
+You may provide multiple letters.
+
+**Example Usage**
+
+``[p]nato adh``
+``[p]nato a, p, q``
+
+**Arguments**
+
+* ``<letters...>``: The letters to convert to NATO.
+
+----------------------
+Additional Information
+----------------------
+
+This cog has been vetted by the Red-DiscordBot QA team as approved.
+For inquiries, see to the contact options below.
+
+---------------
+Receive Support
+---------------
+
+Feel free to ping me at the `Red Cog Support Server <https://discord.gg/GET4DVk>`_ in :code:`#support_othercogs`,
+or you can head over to `my support server <https://discord.gg/JmCFyq7>`_ and ask your questions in :code:`#support-kreusadacogs`.
---
+++ alphanato/alphanato.py
@@ -0,0 +1,71 @@
+"""
+MIT License
+Copyright (c) 2021 kreusada
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import discord
+
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import box
+
+from .alphabet import NATO_ALPHABET
+
+def _remove_whitespace(let):
+    return let.replace(' ','')
+
+
+class AlphaNato(commands.Cog):
+    """
+    Get the names of the NATO phonetics through easy-to-use syntax.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def nato(self, ctx, *, letter: str):
+        """
+        Get the nato phonetic name from a letter.
+
+        You may provide multiple letters.
+        NOTE: Use `[p]nato all` to get all the NATO phonetics.
+
+        **Example Usage:**
+        `[p]nato a, b, c`
+        `[p]nato agz`
+        `[p]nato z`
+        `[p]nato all`
+
+        **Returns:**
+        The NATO alphabet name for the provided characters.
+        """
+        factory = {}
+        if letter.lower() == 'all':
+            for x in NATO_ALPHABET:
+                factory[x[0].lower()] = x
+            msg = "\n".join("'{}' = {}".format(k, v) for k, v in factory.items())
+        else:
+            for x in NATO_ALPHABET:
+                for let in tuple(_remove_whitespace(letter)):
+                    if x[0].lower() == let and x.isalpha():
+                        factory[let] = x
+                    # X-Ray is weird, so this is required.
+                    elif let.lower() == 'x':
+                        factory['x'] = NATO_ALPHABET[-3]
+            msg = "\n".join("'{}' = {}".format(k, v) for k, v in sorted(factory.items()))
+        await ctx.send(box(msg, lang='ml'))
