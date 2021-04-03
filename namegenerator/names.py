283626b041063eb998bd18ff823463b75dedf1b6
Kreusada <kreusadaprojects@gmail.com>
2021-04-03 15:13:03 +0100

    [NameGenerator] Remove mashu, code cleanup

--- namegenerator/names.py
+++ namegenerator/names.py
@@ -14,14 +14,13 @@ class NameGenerator(commands.Cog):
     Generates random names.
     """
 
-    __author__ = ["Kreusada", ]
-    __version__ = "2.0.0"
+    __author__ = ["Kreusada"]
+    __version__ = "2.0.2"
 
     def __init__(self, bot):
         self.bot = bot
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
-        """Thanks Sinbad."""
         context = super().format_help_for_context(ctx)
         authors = ", ".join(self.__author__)
         return f"{context}\n\nAuthor: {authors}\nVersion: {self.__version__}"
@@ -30,8 +29,8 @@ class NameGenerator(commands.Cog):
     async def name(self, ctx):
         """Commands for NameGenerator."""
 
-    @name.command(name="full")
-    async def _full(self, ctx: commands.Context, gender: str = None):
+    @name.command()
+    async def full(self, ctx: commands.Context, gender: str = None):
         """
         Generates a full name.
 
@@ -43,8 +42,8 @@ class NameGenerator(commands.Cog):
         else:
             await ctx.send(full())
 
-    @name.command(name="first")
-    async def _first(self, ctx: commands.Context, gender: str = None):
+    @name.command()
+    async def first(self, ctx: commands.Context, gender: str = None):
         """
         Generates a first name.
 
@@ -56,8 +55,8 @@ class NameGenerator(commands.Cog):
         else:
             await ctx.send(first())
 
-    @name.command(name="last")
-    async def _last(self, ctx: commands.Context):
+    @name.command()
+    async def last(self, ctx: commands.Context):
         """
         Generates a last name.
 
@@ -73,17 +72,4 @@ class NameGenerator(commands.Cog):
         """
         a = word1
         b = word2
-        await ctx.send(a[:len(a) // 2].strip() + b[len(b) // 2:].strip())
-
-    @name.command()
-    async def mashu(self, ctx, member1: discord.Member, member2: discord.Member, use_nicks: bool = False):
-        """
-        Mash two usernames together.
-        """
-        if use_nicks:
-            a = member1.display_name
-            b = member2.display_name
-        else:
-            a = member1.name
-            b = member2.name
         await ctx.send(a[:len(a) // 2].strip() + b[len(b) // 2:].strip())
