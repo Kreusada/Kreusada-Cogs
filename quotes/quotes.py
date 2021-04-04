0ddf232d666ea3032b4812a454b4c09a35694ab3
Kreusada <kreusadaprojects@gmail.com>
2021-04-04 11:29:10 +0100

    [Quotes] Extra required functions

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -7,10 +7,22 @@ from redbot.core.utils.chat_formatting import bold
 class Quotes(commands.Cog):
     """Get a random quote."""
 
+    __version__ = "1.0.0"
+    __author__ = ["Kreusada"]
+
     def __init__(self, bot):
         self.bot = bot
         self.url = 'https://api.quotable.io/random'
         self.session = aiohttp.ClientSession()
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        context = super().format_help_for_context(ctx)
+        authors = ", ".join(self.__author__)
+        return f"{context}\n\nAuthor: {authors}\nVersion: {self.__version__}"
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
 
     def formatter(self, content):
         header = bold("From {}")
