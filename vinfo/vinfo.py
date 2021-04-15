4537d38c4f3f4503a608ffcec2bebc64bd71547f
Kreusada <67752638+Kreusada@users.noreply.github.com>
2021-04-15 09:26:30 +0100

    [Vinfo] Breaking fix for joining integers

--- vinfo/vinfo.py
+++ vinfo/vinfo.py
@@ -75,7 +75,7 @@ class Vinfo(commands.Cog):
         for attr in attrs_to_check:
             if hasattr(module, attr) and check_isinstance(module, attr):
                 return [getattr(module, attr), "." + attr]
-        if module.__name__ in stdlib_list(".".join(sys.version_info[:2])):
+        if module.__name__ in stdlib_list(".".join([str(x) for x in sys.version_info[:2]])):
             # Will bump on Red python bump, eventually
             return builtin
         # if hasattr(module, '__file__'):
