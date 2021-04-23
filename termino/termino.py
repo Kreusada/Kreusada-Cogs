53c3646e213bd1a2b3110ca0c86a279845555eee
Kreusada <67752638+Kreusada@users.noreply.github.com>
2021-04-23 18:28:30 +0100

    ui update

--- termino/termino.py
+++ termino/termino.py
@@ -185,7 +185,8 @@ class Termino(commands.Cog):
             f"Shutdown confirmation: {config['confirm_shutdown']}\n\n"
             f"Restart message: {config['restart_message']}\n"
             f"Restart confirmation: {config['confirm_restart']}\n"
-            f"Restarted message (message sent when a successful restart has occurred): {config['restarted_message']}\n\n"
+            f"Restarted message: {config['restarted_message']}\n
+            f"\t- NOTE: This message will be sent in the invoked channel when a successful restart has occured.\n\n"
         )
         if footer:
             message += "{author} will be replaced with the display name of the invoker."
