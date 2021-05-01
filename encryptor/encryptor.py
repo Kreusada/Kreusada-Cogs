101535664fd923f6e9871fbcf05ea107f41f3530
Kreusada <kreusadaprojects@gmail.com>
2021-05-01 23:48:41 +0100

    [Encryptor] Add important red functions

--- encryptor/encryptor.py
+++ encryptor/encryptor.py
@@ -17,8 +17,20 @@ class Encryptor(commands.Cog):
     Create, and validify the strength of passwords.
     """
 
+    __author__ = ["Kreusada"]
+    __version__ = "1.0.0"
+
     def __init__(self, bot):
         self.bot = bot
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        context = super().format_help_for_context(ctx)
+        authors = ", ".join(self.__author__)
+        return f"{context}\n\nAuthor: {authors}\nVersion: {self.__version__}"
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete"""
+        return
 
     @commands.group()
     async def password(self, ctx):
