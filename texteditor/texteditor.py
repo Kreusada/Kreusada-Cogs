0bc2bc2ee04e7e13d0945ddb7907677b4d8cf453
Kreusada <kreusadaprojects@gmail.com>
2021-06-01 17:26:05 +0100

    Rename cog, and docs. Woohoo! :|

--- texteditor/texteditor.py
+++ texteditor/texteditor.py
@@ -10,9 +10,9 @@ from redbot.core.utils.chat_formatting import bold, box
 # TODO: Rewrite
 
 
-class TextManipulator(commands.Cog):
+class TextEditor(commands.Cog):
     """
-    Manipulate characters and text.
+    Edit and manipulate with text.
     """
 
     __author__ = ["Kreusada"]
@@ -42,19 +42,19 @@ class TextManipulator(commands.Cog):
                 self.bot.add_dev_env_value("textmanipulator", lambda x: self)
 
     @commands.group()
-    async def tm(self, ctx: commands.Context):
-        """Base command for manipulating with text."""
+    async def editor(self, ctx: commands.Context):
+        """Base command for editting text."""
 
-    @tm.command(name="charcount")
-    async def tm_charcount(self, ctx: commands.Context, include_spaces: Optional[bool] = True, *, text: str):
+    @editor.command(name="charcount")
+    async def editor_charcount(self, ctx: commands.Context, include_spaces: Optional[bool] = True, *, text: str):
         """Count the number of characters appearing in the text."""
         if include_spaces:
             await ctx.send("Character count (with spaces): {}".format(bold(str(len(text)))))
         else:
             await ctx.send("Character count: {}".format(bold(str(len(text) - text.count(" ")))))
 
-    @tm.command(name="wordcount")
-    async def tm_wordcount(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="wordcount")
+    async def editor_wordcount(self, ctx: commands.Context, *, text: str):
         """Count the number of words appearing in the text."""
         count = 0
         for word in text.split():
@@ -63,44 +63,44 @@ class TextManipulator(commands.Cog):
             count += 1
         await ctx.send("Word count: {}".format(bold(str(count))))
 
-    @tm.command(name="occurance")
-    async def tm_occurance(self, ctx: commands.Context, check: str, *, text: str):
+    @editor.command(name="occurance")
+    async def editor_occurance(self, ctx: commands.Context, check: str, *, text: str):
         """Count how many times something appears in the text."""
         counter = text.count(check)
         plural_check = "once" if counter == 1 else f"{counter} times"
         await ctx.send("`{}` is present in the text {}.".format(str(check), bold(plural_check)))
 
-    @tm.command(name="upper")
-    async def tm_upper(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="upper")
+    async def editor_upper(self, ctx: commands.Context, *, text: str):
         """Convert the text to uppercase."""
         await ctx.send(box(text.upper()))
 
-    @tm.command(name="lower")
-    async def tm_lower(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="lower")
+    async def editor_lower(self, ctx: commands.Context, *, text: str):
         """Convert the text to lowercase."""
         await ctx.send(box(text.lower()))
 
-    @tm.command(name="title")
-    async def tm_title(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="title")
+    async def editor_title(self, ctx: commands.Context, *, text: str):
         """Convert the text to titlecase."""
         await ctx.send(box(text.title()))
 
-    @tm.command(name="snake")
-    async def tm_snake(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="snake")
+    async def editor_snake(self, ctx: commands.Context, *, text: str):
         """Convert all spaces to underscores."""
         snake = lambda x: x.replace(" ", "_")
         await ctx.send(box(snake(text)))
 
-    @tm.command(name="alternating")
-    async def tm_alternating(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="alternating")
+    async def editor_alternating(self, ctx: commands.Context, *, text: str):
         """Convert the text to alternating case."""
         text = list(text)
         text[0::2] = map(str.upper, text[0::2])
         text[1::2] = map(str.lower, text[1::2])
         await ctx.send(box("".join(text)))
 
-    @tm.command(name="replace")
-    async def tm_replace(
+    @editor.command(name="replace")
+    async def editor_replace(
         self, 
         ctx: commands.Context, 
         text_to_replace: str,
@@ -112,29 +112,29 @@ class TextManipulator(commands.Cog):
         replace = lambda x: x.replace(text_to_replace, replacement)
         await ctx.send(box(replace(text)))
 
-    @tm.command(name="squash")
-    async def tm_squash(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="squash")
+    async def editor_squash(self, ctx: commands.Context, *, text: str):
         """Squash all the words into one."""
         await ctx.send(box(text.replace(" ", "")))
 
-    @tm.command(name="remove")
-    async def tm_remove(self, ctx: commands.Context, remove: str, *, text: str):
+    @editor.command(name="remove")
+    async def editor_remove(self, ctx: commands.Context, remove: str, *, text: str):
         """Remove something from the text."""
         await ctx.send(box(text.replace(remove, "")))
 
-    @tm.command(name="trim", aliases=["strip"])
-    async def tm_trim(self, ctx: commands.Context, trimmer: Optional[str] = " ", *, text: str):
+    @editor.command(name="trim", aliases=["strip"])
+    async def editor_trim(self, ctx: commands.Context, trimmer: Optional[str] = " ", *, text: str):
         """Trim the outskirts of the text."""
         await ctx.send(box(text.strip(trimmer)))
 
-    @tm.command(name="shuffle", aliases=["jumble"])
-    async def tm_shuffle(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="shuffle", aliases=["jumble"])
+    async def editor_shuffle(self, ctx: commands.Context, *, text: str):
         """Completely shuffle the text."""
         data = text.split()
         random.shuffle(data)
         await ctx.send(box(" ".join(data)))
 
-    @tm.command(name="reverse")
-    async def tm_reverse(self, ctx: commands.Context, *, text: str):
+    @editor.command(name="reverse")
+    async def editor_reverse(self, ctx: commands.Context, *, text: str):
         """Reverse the text."""
         await ctx.send(box(text[::-1]))
