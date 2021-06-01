b1d22761ad2453b7c8a631ba1d0282c9441dce31
Kreusada <kreusadaprojects@gmail.com>
2021-06-01 11:57:14 +0100

    Initial commit

--- textmanipulator/textmanipulator.py
+++ textmanipulator/textmanipulator.py
@@ -1,7 +1,11 @@
 import contextlib
+from typing import Optional
 
 import discord
+import random
+
 from redbot.core import commands
+from redbot.core.utils.chat_formatting import bold, box
 
 # TODO: Rewrite
 
@@ -12,7 +16,7 @@ class TextManipulator(commands.Cog):
     """
 
     __author__ = ["Kreusada"]
-    __version__ = "1.5.1"
+    __version__ = "2.0.0"
 
     def __init__(self, bot):
         self.bot = bot
@@ -38,50 +42,99 @@ class TextManipulator(commands.Cog):
                 self.bot.add_dev_env_value("textmanipulator", lambda x: self)
 
     @commands.group()
-    async def convert(self, ctx: commands.Context):
-        """Convert text into different types."""
+    async def tm(self, ctx: commands.Context):
+        """Base command for manipulating with text."""
 
-    @commands.group()
-    async def count(self, ctx: commands.Context):
-        """Count the number of characters and words."""
+    @tm.command(name="charcount")
+    async def tm_charcount(self, ctx: commands.Context, include_spaces: Optional[bool] = True, *, text: str):
+        """Count the number of characters appearing in the text."""
+        if include_spaces:
+            await ctx.send("Character count (with spaces): {}".format(bold(str(len(text)))))
+        else:
+            await ctx.send("Character count: {}".format(bold(str(len(text) - text.count(" ")))))
 
-    @count.command(aliases=["char"])
-    async def characters(self, ctx: commands.Context, *, characters: str):
-        """Count how many characters are in a specified text."""
-        await ctx.send(
-            f"**Including spaces:** {str(len(characters))}\n"
-            f"**Excluding spaces:** {str(len(characters) - characters.count(' '))}`"
-        )
+    @tm.command(name="wordcount")
+    async def tm_wordcount(self, ctx: commands.Context, *, text: str):
+        """Count the number of words appearing in the text."""
+        count = 0
+        for word in text.split():
+            if word.isspace():
+                continue
+            count += 1
+        await ctx.send("Word count: {}".format(bold(str(count))))
 
-    @count.command()
-    async def words(self, ctx: commands.Context, *, words: str):
-        """Count how many words are in a specified text."""
-        await ctx.send(f"**Total words:** {str(len(words.split()))}")
+    @tm.command(name="occurance")
+    async def tm_occurance(self, ctx: commands.Context, check: str, *, text: str):
+        """Count how many times something appears in the text."""
+        counter = text.count(check)
+        plural_check = "once" if counter == 1 else f"{counter} times"
+        await ctx.send("`{}` is present in the text {}.".format(str(check), bold(plural_check)))
 
-    @convert.command()
-    async def upper(self, ctx: commands.Context, *, characters: str):
-        """Convert all characters to uppercase."""
-        await ctx.send(characters.upper())
+    @tm.command(name="upper")
+    async def tm_upper(self, ctx: commands.Context, *, text: str):
+        """Convert the text to uppercase."""
+        await ctx.send(box(text.upper()))
 
-    @convert.command()
-    async def lower(self, ctx: commands.Context, *, characters: str):
-        """Convert all characters to lowercase."""
-        await ctx.send(characters.lower())
+    @tm.command(name="lower")
+    async def tm_lower(self, ctx: commands.Context, *, text: str):
+        """Convert the text to lowercase."""
+        await ctx.send(box(text.lower()))
 
-    @convert.command()
-    async def title(self, ctx: commands.Context, *, characters: str):
-        """Convert all characters to titlecase."""
-        await ctx.send(characters.title())
+    @tm.command(name="title")
+    async def tm_title(self, ctx: commands.Context, *, text: str):
+        """Convert the text to titlecase."""
+        await ctx.send(box(text.title()))
 
-    @convert.command()
-    async def snake(self, ctx: commands.Context, *, characters: str):
+    @tm.command(name="snake")
+    async def tm_snake(self, ctx: commands.Context, *, text: str):
         """Convert all spaces to underscores."""
-        await ctx.send(characters.replace(" ", "_"))
+        snake = lambda x: x.replace(" ", "_")
+        await ctx.send(box(snake(text)))
 
-    @convert.command()
-    async def alt(self, ctx: commands.Context, *, characters: str):
-        """Convert all characters to alternating case."""
-        characters = list(characters)
-        characters[0::2] = map(str.upper, characters[0::2])
-        characters[1::2] = map(str.lower, characters[1::2])
-        await ctx.send("".join(characters))
+    @tm.command(name="alternating")
+    async def tm_alternating(self, ctx: commands.Context, *, text: str):
+        """Convert the text to alternating case."""
+        text = list(text)
+        text[0::2] = map(str.upper, text[0::2])
+        text[1::2] = map(str.lower, text[1::2])
+        await ctx.send(box("".join(text)))
+
+    @tm.command(name="replace")
+    async def tm_replace(
+        self, 
+        ctx: commands.Context, 
+        text_to_replace: str,
+        replacement: str,
+        *, 
+        text: str
+    ):
+        """Replace certain parts of the text."""
+        replace = lambda x: x.replace(text_to_replace, replacement)
+        await ctx.send(box(replace(text)))
+
+    @tm.command(name="squash")
+    async def tm_squash(self, ctx: commands.Context, *, text: str):
+        """Squash all the words into one."""
+        await ctx.send(box(text.replace(" ", "")))
+
+    @tm.command(name="remove")
+    async def tm_remove(self, ctx: commands.Context, remove: str, *, text: str):
+        """Remove something from the text."""
+        await ctx.send(box(text.replace(remove, "")))
+
+    @tm.command(name="trim", aliases=["strip"])
+    async def tm_trim(self, ctx: commands.Context, trimmer: Optional[str] = " ", *, text: str):
+        """Trim the outskirts of the text."""
+        await ctx.send(box(text.strip(trimmer)))
+
+    @tm.command(name="shuffle", aliases=["jumble"])
+    async def tm_shuffle(self, ctx: commands.Context, *, text: str):
+        """Completely shuffle the text."""
+        data = text.split()
+        random.shuffle(data)
+        await ctx.send(box(" ".join(data)))
+
+    @tm.command(name="reverse")
+    async def tm_reverse(self, ctx: commands.Context, *, text: str):
+        """Reverse the text."""
+        await ctx.send(box(text[::-1]))
