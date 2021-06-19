55fa1d8464aafa9d881e0894ba3ae094b9daf25d
Kreusada <kreusadaprojects@gmail.com>
2021-06-19 20:40:29 +0100

    Fixes

--- raffle/versioninfo/components.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-RAW_VERSION_PATH = Path(__file__).parent / "version.py"
-RAW_VERSION_URL = "https://raw.githubusercontent.com/Kreusada/Kreusada-Cogs/master/raffle/versioninfo/version.py"
--- raffle/versioninfo/version_handler.py
+++ raffle/versioninfo/version_handler.py
@@ -1,29 +1,35 @@
 import ast
 import aiohttp
+import json
+
+from pathlib import Path
 from typing import Union, Tuple
 
-from .components import RAW_VERSION_PATH, RAW_VERSION_URL
+RAW_VERSION_PATH = Path(__file__).parent.parent / "info.json"
+RAW_VERSION_URL = "https://raw.githubusercontent.com/Kreusada/Kreusada-Cogs/master/raffle/info.json"
+
 
 class VersionHandler(object):
     """A raffle version information object used to
     check if the cog is outdated."""
 
+    with open(RAW_VERSION_PATH, "r") as f:
+        __version__ = json.load(f)["version"]
+
     @classmethod
     def versiongetter(cls, asstr: bool = True) -> Union[str, Tuple[int, ...]]:
-        with open(RAW_VERSION_PATH) as f:
-            ret = ast.literal_eval(f.readlines()[0].rstrip())
         if asstr:
-            return ".".join(str(i) for i in ret)
-        return ret
+            return ".".join(str(i) for i in cls.__version__)
+        return cls.__version__
 
     @classmethod
     async def rawversiongetter(cls, asstr: bool = True) -> Union[str, Tuple[int, ...]]:
         async with aiohttp.request("GET", RAW_VERSION_URL) as res:
             ret = await res.text()
-        ret = ast.literal_eval(ret)
+        ret = json.loads(ret)["version"]
         if asstr:
             return ".".join(str(i) for i in ret)
-        return ret
+        return tuple(ret)
 
     @classmethod
     async def validate(cls) -> bool:
