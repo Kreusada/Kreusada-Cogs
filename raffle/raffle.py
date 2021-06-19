34c60e3c1a9efc897748513fa7550843bf36920c
Kreusada <kreusadaprojects@gmail.com>
2021-06-19 11:29:12 +0100

    remove imports

--- raffle/raffle.py
+++ raffle/raffle.py
@@ -62,9 +62,6 @@ from .helpers import (
     start_interactive_message_session
 )
 
-from .mixins.abc import RaffleMixin
-from .mixins.metaclass import MetaClass
-
 
 with open(pathlib.Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
