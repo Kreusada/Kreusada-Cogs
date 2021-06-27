af76e34fd806c779d274186cc11bc8f2d877a49f
Kreusada <kreusadaprojects@gmail.com>
2021-06-27 09:23:39 +0100

    [Flags] Use converter for arguments

--- flags/flags.py
+++ flags/flags.py
@@ -7,9 +7,11 @@ try:
     import tabulate
 except ModuleNotFoundError:
     tabulate = None
+    
+from typing import Dict, Union
 
 from redbot.core import commands
-from redbot.core.commands import Cog, Context
+from redbot.core.commands import BadArgument, Cog, Context, Converter
 from redbot.core.utils.chat_formatting import box, humanize_list
 
 
@@ -21,6 +23,68 @@ def format_attr(t):
     return t.replace('_',' ').title()
 
 
+EXCEPTIONS = {"russia": "ru"}
+IMAGE_BASE = "https://flagpedia.net/data/flags/w580/{}.png"
+
+
+class Country(Converter):
+    """country converter"""
+
+    async def convert(self, ctx: Context, argument: str) -> Union[Dict[str, Union[str, int]], None]:
+        argument = argument.lower()
+        get = lambda **kwargs: pycountry.countries.get(**kwargs)
+
+        if argument in ("england", "scotland", "wales"):
+
+            special_images = {
+                "england": "gb-eng",
+                "wales": "gb-wls",
+                "scotland": "gb-sct"
+            }
+
+            if tabulate:
+                description = box(f"Emoji Information  [:{argument}:]", lang="ini")
+            else:
+                description = box(f"Emoji Information: :{argument}:", lang="yaml")
+
+            emoji = f":{argument}:"
+            country_name = argument.title()
+            image = IMAGE_BASE.format(special_images[argument])
+            name = country_name.title()
+
+            return {
+                "description": description,
+                "emoji": emoji,
+                "name": square(country_name.title()),
+                "title": f"{emoji} {name}",
+                "image": image
+            }
+
+        else:
+
+            obj = get(name=argument) or get(alpha_2=argument)
+            if not obj:
+                obj = None
+                for k, v in EXCEPTIONS.items():
+                    if k in argument:
+                        obj = get(alpha_2=v)
+                        break
+                if not obj:
+                    raise BadArgument("Could not match this argument to a country.")
+            
+            ret = {
+                "name": square(obj.name.title()), 
+                "title": f":flag_{obj.alpha_2.lower()}: {obj.name}",
+                "emoji": square(f":flag_{obj.alpha_2.lower()}:"),
+                "image": IMAGE_BASE.format(obj.alpha_2.lower())
+            }
+
+            for attr in ("alpha_2", "alpha_3", "numeric", "official_name"):
+                if hasattr(obj, attr):
+                    ret[attr] = square(getattr(obj, attr))
+                
+            return ret
+
 class Flags(Cog):
     """Get flags from country names."""
     
@@ -29,8 +93,6 @@ class Flags(Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.image_base = "https://flagpedia.net/data/flags/w580/{}.png"
-        self.exceptions = {"russia": "ru"}
 
 
     def format_help_for_context(self, ctx: Context) -> str:
@@ -56,7 +118,7 @@ class Flags(Cog):
 
 
     @commands.command()
-    async def flag(self, ctx: Context, *, argument: str):
+    async def flag(self, ctx: Context, *, argument: Country):
         """Get the flag for a country.
         
         Either the country name or alpha 2 code can be provided.
@@ -68,61 +130,27 @@ class Flags(Cog):
             - ``[p]flag dk``
             - ``[p]flag se``
         """
-        argument = argument.lower()
-        get = lambda **kwargs: pycountry.countries.get(**kwargs)
+        description = argument.get("description", None)
+        image = argument["image"]
+        title = argument["title"]
 
-        if argument in ("england", "scotland", "wales"):
+        del argument["image"]
+        del argument["title"]
 
-            special_images = {
-                "england": "gb-eng",
-                "wales": "gb-wls",
-                "scotland": "gb-sct"
-            }
-
+        if description is None:
             if tabulate:
-                description = box(f"Emoji Information  [:{argument}:]", lang="ini")
+                description = box(tabulate.tabulate([(format_attr(k), v) for k, v in argument.items()], tablefmt="plain"), lang="ini")
             else:
-                description = box(f"Emoji Information: :{argument}:", lang="yaml")
-            emoji = f":{argument}:"
-            country_name = argument.title()
-            image = self.image_base.format(special_images[argument])
+                description = box((f"{format_attr(k)}: {v}" for k, v in argument.items()), lang="yaml")
 
-        else:
-
-            obj = get(name=argument) or get(alpha_2=argument)
-            if not obj:
-                obj = None
-                for k, v in self.exceptions.items():
-                    if k in argument:
-                        obj = get(alpha_2=v)
-                        break
-                if not obj:
-                    return await ctx.send("Could not match this argument to a country.")
-
-            data = []
-
-            if hasattr(obj, "alpha_2"):
-                emoji = f":flag_{obj.alpha_2.lower()}:"
-                data.append(("Emoji Information", square(emoji)))
-
-            for attr in ("alpha_2", "alpha_3", "numeric", "official_name"):
-                if hasattr(obj, attr):
-                    data.append((format_attr(attr), square(getattr(obj, attr))))
-
-            if tabulate:
-                description = box(tabulate.tabulate(data, tablefmt="plain"), lang="ini")
-            else:
-                description = box((f"{v[0]}: {v[1]}" for v in data), lang="yaml")
-
-            country_name = obj.name
-            image = self.image_base.format(obj.alpha_2.lower())
 
 
         embed = discord.Embed(
-            title=f"{emoji} {country_name}",
+            title=title,
             description=description,
             color=await ctx.embed_colour(),
             timestamp=datetime.datetime.now()
         )
+
         embed.set_image(url=image)
         await ctx.send(embed=embed)
