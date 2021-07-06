e24c5909fdfb1c968a1298f92f1c6eb352f1966d
Kreusada <kreusadaprojects@gmail.com>
2021-07-06 12:17:03 +0100

    initial commit

--- roleboards/roleboards.py
+++ roleboards/roleboards.py
@@ -3,7 +3,22 @@ import contextlib
 import discord
 from redbot.core import commands, checks
 from redbot.core.utils.chat_formatting import pagify, box
+from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from tabulate import tabulate
+
+
+class ValidRoleIndex(commands.Converter):
+    async def convert(self, ctx: commands.Context, argument: int):
+        if argument > len(ctx.guild.roles):
+            raise commands.BadArgument("Please provide an index lower than the number of roles in this guild.")
+        return argument
+
+    
+class ValidUserIndex(commands.Converter):
+    async def convert(self, ctx: commands.Context, argument: int):
+        if argument > len(ctx.guild.members):
+            raise commands.BadArgument("Please provide an index lower than the number of users in this guild.")
+        return argument
 
 
 class RoleBoards(commands.Cog):
@@ -12,35 +27,43 @@ class RoleBoards(commands.Cog):
     the roles with the most users, and a full list of all the roles.
     """
 
+
     __author__ = ["Kreusada"]
     __version__ = "2.0.1"
 
+
     def __init__(self, bot):
         self.bot = bot
+
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         context = super().format_help_for_context(ctx)
         authors = ", ".join(self.__author__)
         return f"{context}\n\nAuthor: {authors}\nVersion: {self.__version__}"
 
+
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete."""
         return
 
+
     def cog_unload(self):
         with contextlib.suppress(Exception):
             self.bot.remove_dev_env_value("roleboards")
+
 
     async def initialize(self) -> None:
         if 719988449867989142 in self.bot.owner_ids:
             with contextlib.suppress(Exception):
                 self.bot.add_dev_env_value("roleboards", lambda x: self)
 
+
     @commands.group(aliases=["rb"])
     @commands.guild_only()
     async def roleboard(self, ctx):
         """Get roleboards for this server.."""
         pass
+
 
     @roleboard.command()
     async def listroles(self, ctx: commands.Context):
@@ -61,36 +84,73 @@ class RoleBoards(commands.Cog):
         for page in pagify(data, page_length=1990):
             await ctx.send(box(page, lang="cs"))
 
+
     @roleboard.command()
-    async def topusers(self, ctx, index: int = 20):
+    async def topusers(self, ctx, index: ValidUserIndex):
         """Get the users with the most roles."""
         g = ctx.guild
         data = self.get_users(g, index)
-        message = "\n".join(f"#{self.td(c)} [{self.td(v[1])}] {v[0]}" for c, v in enumerate(data, 1))
-        for page in pagify(message, page_length=1990):
-            await ctx.send(box(message, lang="css"))
+
+        pages = []
+        enum = 1
+
+        for sector in data:
+            embed = discord.Embed(
+                title="Users with the most roles",
+                description=box("\n".join(f"#{self.td(c)} [{self.td(v[1])}] {v[0]}" for c, v in enumerate(sector, enum)), lang="css"),
+                color=await ctx.embed_colour()
+            )
+
+            embed.set_footer(text=f"Page {data.index(sector)+1}/{len(data)}")
+            pages.append(embed)
+            enum += 10
+
+        await menu(ctx, pages, DEFAULT_CONTROLS)
+
 
     @roleboard.command()
-    async def toproles(self, ctx, index: int = 20):
+    async def toproles(self, ctx, index: ValidRoleIndex):
         """Get the roles with the most users."""
         g = ctx.guild
         data = self.get_roles(g, index)
-        message = "\n".join(f"#{self.td(c)} [{self.td(v[1])}] {v[0]}" for c, v in enumerate(data, 1))
-        for page in pagify(message, page_length=1990):
-            await ctx.send(box(message, lang="css"))
 
-    @staticmethod
-    def get_users(guild: discord.Guild, index: int):
+        pages = []
+        enum = 1
+
+        for sector in data:
+            embed = discord.Embed(
+                title="Roles with the most users",
+                description=box("\n".join(f"#{self.td(c)} [{self.td(v[1])}] {v[0]}" for c, v in enumerate(sector, enum)), lang="css"),
+                color=await ctx.embed_colour()
+            )
+
+            embed.set_footer(text=f"Page {data.index(sector)+1}/{len(data)}")
+            pages.append(embed)
+            enum += 10
+
+        await menu(ctx, pages, DEFAULT_CONTROLS)
+
+
+    def get_users(self, guild: discord.Guild, index: int):
         key = lambda x: len(x.roles)
         top_members = sorted([x for x in guild.members], key=key, reverse=True)
-        return [(x.display_name, len(x.roles) - 1) for x in top_members[:index]]
+        data = [(x.display_name, len(x.roles) - 1) for x in top_members[:index]]
+        return list(self.yield_chunks(data, 10))
 
-    @staticmethod
-    def get_roles(guild: discord.Guild, index: int):
+
+    def get_roles(self, guild: discord.Guild, index: int):
         key = lambda x: len(x.members)
         top_roles = sorted([r for r in guild.roles], key=key, reverse=True)[1:]
-        return [(x.name, len(x.members)) for x in top_roles[:index]]
+        data = [(x.name, len(x.members)) for x in top_roles[:index]]
+        return list(self.yield_chunks(data, 10))
+
 
     @staticmethod
     def td(item):
         return f"0{item}" if len(str(item)) == 1 else item
+
+
+    @staticmethod
+    def yield_chunks(l, n):
+        for i in range(0,len(l),n):
+            yield l[i:i+n]
