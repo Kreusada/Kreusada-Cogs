e96fc6401f0757ad0bef7e879d9c67c95db73508
Kreusada <67752638+Kreusada@users.noreply.github.com>
2021-07-07 11:39:06 +0100

    [Meta] Allow isort, black and stylediffs to be ran separately (#140)
    * [Meta] Allow isort, black and stylediffs to be ran separately
    
    * newline
    
    * Fixes
    
    * remove venv variable calls

--- make.bat
+++ make.bat
@@ -1,17 +1,29 @@
 @echo off
 
+CALL %userprofile%\redenv\Scripts\activate.bat REM win
 if [%1] == [] goto help
 
 REM This allows us to expand variables at execution
 setlocal ENABLEDELAYEDEXPANSION
 
-REM Edit after venv= for your own venv
-set venv=%userprofile%\redenv\Scripts\activate.bat
 goto %1
 
 :reformat
-%venv% isort .
-%venv% black .
+isort .
+black .
+exit /B %ERRORLEVEL%
+
+:isort
+isort .
+exit /B %ERRORLEVEL%
+
+:black
+black .
+exit /B %ERRORLEVEL%
+
+:stylediff
+isort --atomic --check --diff --line-length 99 --use-parentheses .
+black --check --diff -l 99 .
 exit /B %ERRORLEVEL%
 
 :help
@@ -20,3 +32,6 @@ echo   make ^<command^>
 echo.
 echo Commands:
 echo   reformat                   Reformat all .py files being tracked by git.
+echo   isort                      Reformat all .py files only with isort.
+echo   black                      Reformat all .py files only with black.
+echo   stylediff                  Check .py files for style diffs.
