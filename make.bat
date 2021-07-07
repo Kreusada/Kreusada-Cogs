7a4ed5ecbf753c286b860e40b57582994adfc31a
Kreusada <kreusadaprojects@gmail.com>
2021-07-07 11:21:05 +0100

    [Meta] Allow isort, black and stylediffs to be ran separately

--- make.bat
+++ make.bat
@@ -14,9 +14,25 @@ goto %1
 %venv% black .
 exit /B %ERRORLEVEL%
 
+:isort
+%venv% isort .
+exit /B %ERRORLEVEL%
+
+:black
+%venv% black .
+exit /B %ERRORLEVEL%
+
+:stylediff
+%venv% isort --atomic --check --diff --line-length 99 --use-parentheses .
+%venv% black --check --diff -l 99 .
+exit /B %ERRORLEVEL%
+
 :help
 echo Usage:
 echo   make ^<command^>
 echo.
 echo Commands:
 echo   reformat                   Reformat all .py files being tracked by git.
+echo   isort                      Reformat all .py files only with isort.
+echo   black                      Reformat all .py files only with black.
+echo   stylediff                  Check .py files for style diffs.
