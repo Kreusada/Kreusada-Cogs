2b649c9ea82c60c6a051a8b01f6b4eb13e26547e
Kreusada <kreusadaprojects@gmail.com>
2021-07-13 17:55:17 +0100

    [TextEditor] v3.0.0 Main Editor class (don't ask why :P)

--- texteditor/texteditor.py
+++ texteditor/texteditor.py
@@ -1,10 +1,57 @@
 import contextlib
+import inspect
 import random
 from typing import Optional
 
-import discord
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import bold, box
+
+
+class Editor(str):
+    """A main class used for the helper functions."""
+
+    def _charcount(self, count_spaces: bool):
+        if count_spaces:
+            return f"Character count (with spaces): {bold(str(len(self)))}"
+        return f"Character count: {bold(len(self) - self.count(' '))}"
+
+    def _wordcount(self):
+        return f"Word count: {len(self.split() - self.count(' '))}"
+
+    def _occurance(self, check):
+        counter = self.count(check)
+        plural_check = "once" if counter == 1 else f"{counter} times"
+        return f"`{check}` is present in the text {bold(plural_check)}."
+
+    def _snake(self):
+        return box(self.replace(" ", "_"))
+
+    def _alternating(self):
+        text = list(self)
+        text[0::2] = map(str.upper, text[0::2])
+        text[1::2] = map(str.lower, text[1::2])
+        return box(text)
+
+    def _replace(self, text_to_replace, replacement):
+        replace = lambda x: x.replace(text_to_replace, replacement)
+        return box(replace(self))
+
+    def _squash(self):
+        return box(self.replace(" ", ""))
+
+    def _remove(self, remove):
+        return box(self.replace(remove, ""))
+
+    def _trim(self, trimmer):
+        return box(self.strip(trimmer).strip())
+
+    def _shuffle(self):
+        data = self.split()
+        random.shuffle(data)
+        return box(" ".join(data))
+
+    def _reverse(self):
+        return box(self[::-1])
 
 
 class TextEditor(commands.Cog):
@@ -13,7 +60,7 @@ class TextEditor(commands.Cog):
     """
 
     __author__ = ["Kreusada"]
-    __version__ = "2.0.0"
+    __version__ = "3.0.0"
 
     def __init__(self, bot):
         self.bot = bot
@@ -38,6 +85,11 @@ class TextEditor(commands.Cog):
             with contextlib.suppress(Exception):
                 self.bot.add_dev_env_value("texteditor", lambda x: self)
 
+    async def format_text(self, ctx, function: str, mainbase: str, *args):
+        base = Editor(mainbase)
+        func = getattr(base, function[6:])
+        return await ctx.send(func(*args))
+
     @commands.group()
     async def editor(self, ctx: commands.Context):
         """Base command for editting text."""
@@ -47,57 +99,68 @@ class TextEditor(commands.Cog):
         self, ctx: commands.Context, include_spaces: Optional[bool] = True, *, text: str
     ):
         """Count the number of characters appearing in the text."""
-        if include_spaces:
-            await ctx.send("Character count (with spaces): {}".format(bold(str(len(text)))))
-        else:
-            await ctx.send("Character count: {}".format(bold(str(len(text) - text.count(" ")))))
+        await self.format_text(ctx, inspect.stack()[0][3], text, include_spaces)
 
     @editor.command(name="wordcount")
     async def editor_wordcount(self, ctx: commands.Context, *, text: str):
         """Count the number of words appearing in the text."""
-        count = 0
-        for word in text.split():
-            if word.isspace():
-                continue
-            count += 1
-        await ctx.send("Word count: {}".format(bold(str(count))))
+        await self.format_text(ctx, inspect.stack()[0][3], text)
 
     @editor.command(name="occurance")
     async def editor_occurance(self, ctx: commands.Context, check: str, *, text: str):
         """Count how many times something appears in the text."""
-        counter = text.count(check)
-        plural_check = "once" if counter == 1 else f"{counter} times"
-        await ctx.send("`{}` is present in the text {}.".format(str(check), bold(plural_check)))
+        await self.format_text(ctx, inspect.stack()[0][3], text, check)
 
     @editor.command(name="upper")
     async def editor_upper(self, ctx: commands.Context, *, text: str):
         """Convert the text to uppercase."""
-        await ctx.send(box(text.upper()))
+        await self.format_text(ctx, inspect.stack()[0][3], text)
 
     @editor.command(name="lower")
     async def editor_lower(self, ctx: commands.Context, *, text: str):
         """Convert the text to lowercase."""
-        await ctx.send(box(text.lower()))
+        await self.format_text(ctx, inspect.stack()[0][3], text)
 
     @editor.command(name="title")
     async def editor_title(self, ctx: commands.Context, *, text: str):
         """Convert the text to titlecase."""
-        await ctx.send(box(text.title()))
+        await self.format_text(ctx, inspect.stack()[0][3], text)
 
     @editor.command(name="snake")
     async def editor_snake(self, ctx: commands.Context, *, text: str):
         """Convert all spaces to underscores."""
-        snake = lambda x: x.replace(" ", "_")
-        await ctx.send(box(snake(text)))
+        await self.format_text(ctx, inspect.stack()[0][3], text)
 
     @editor.command(name="alternating")
     async def editor_alternating(self, ctx: commands.Context, *, text: str):
         """Convert the text to alternating case."""
-        text = list(text)
-        text[0::2] = map(str.upper, text[0::2])
-        text[1::2] = map(str.lower, text[1::2])
-        await ctx.send(box("".join(text)))
+        await self.format_text(ctx, inspect.stack()[0][3], text)
 
+    @editor.command(name="squash")
+    async def editor_squash(self, ctx: commands.Context, *, text: str):
+        """Squash all the words into one."""
+        await self.format_text(ctx, inspect.stack()[0][3], text)
+
+    @editor.command(name="remove")
+    async def editor_remove(self, ctx: commands.Context, remove: str, *, text: str):
+        """Remove something from the text."""
+        await self.format_text(ctx, inspect.stack()[0][3], text, remove)
+
+    @editor.command(name="trim", aliases=["strip"], usage='[trimmer=" "] <text>')
+    async def editor_trim(self, ctx: commands.Context, trimmer: Optional[str] = " ", *, text: str):
+        """Trim the outskirts of the text."""
+        await self.format_text(ctx, inspect.stack()[0][3], text, trimmer)
+
+    @editor.command(name="shuffle", aliases=["jumble"])
+    async def editor_shuffle(self, ctx: commands.Context, *, text: str):
+        """Completely shuffle the text."""
+        await self.format_text(ctx, inspect.stack()[0][3], text)
+
+    @editor.command(name="reverse")
+    async def editor_reverse(self, ctx: commands.Context, *, text: str):
+        """Reverse the text."""
+        await self.format_text(ctx, inspect.stack()[0][3], text)
+        
     @editor.command(name="replace")
     async def editor_replace(
         self,
@@ -108,32 +171,10 @@ class TextEditor(commands.Cog):
         text: str,
     ):
         """Replace certain parts of the text."""
-        replace = lambda x: x.replace(text_to_replace, replacement)
-        await ctx.send(box(replace(text)))
-
-    @editor.command(name="squash")
-    async def editor_squash(self, ctx: commands.Context, *, text: str):
-        """Squash all the words into one."""
-        await ctx.send(box(text.replace(" ", "")))
-
-    @editor.command(name="remove")
-    async def editor_remove(self, ctx: commands.Context, remove: str, *, text: str):
-        """Remove something from the text."""
-        await ctx.send(box(text.replace(remove, "")))
-
-    @editor.command(name="trim", aliases=["strip"], usage='[trimmer=" "] <text>')
-    async def editor_trim(self, ctx: commands.Context, trimmer: Optional[str] = " ", *, text: str):
-        """Trim the outskirts of the text."""
-        await ctx.send(box(text.strip(trimmer).strip()))
-
-    @editor.command(name="shuffle", aliases=["jumble"])
-    async def editor_shuffle(self, ctx: commands.Context, *, text: str):
-        """Completely shuffle the text."""
-        data = text.split()
-        random.shuffle(data)
-        await ctx.send(box(" ".join(data)))
-
-    @editor.command(name="reverse")
-    async def editor_reverse(self, ctx: commands.Context, *, text: str):
-        """Reverse the text."""
-        await ctx.send(box(text[::-1]))
+        await self.format_text(
+            ctx, 
+            inspect.stack()[0][3], 
+            text, 
+            text_to_replace,
+            replacement,
+        )
