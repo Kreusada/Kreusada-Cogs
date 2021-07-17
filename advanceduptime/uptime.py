7ebd451efb3410b02ccb7d866b42e60b8e7bb173
Kreusada <kreusadaprojects@gmail.com>
2021-07-17 22:38:19 +0100

    [AdvancedUptime] v2.0.0 Rewrite with more relevant information

--- advanceduptime/uptime.py
+++ advanceduptime/uptime.py
@@ -1,10 +1,10 @@
 import contextlib
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import discord
 from redbot.core import commands
-from redbot.core.utils.chat_formatting import box, humanize_number, humanize_timedelta
+from redbot.core.utils.chat_formatting import bold, box, humanize_number, humanize_timedelta
 
 log = logging.getLogger("red.kreusada.advanceduptime")
 
@@ -15,10 +15,30 @@ class AdvancedUptime(commands.Cog):
     """
 
     __author__ = ["Kreusada"]
-    __version__ = "1.3.1"
+    __version__ = "2.0.0"
 
     def __init__(self, bot):
         self.bot = bot
+        self.commands_run = {}
+        if 719988449867989142 in self.bot.owner_ids:
+            with contextlib.suppress(Exception):
+                self.bot.add_dev_env_value("advanceduptime", lambda x: self)
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        if await self.bot.cog_disabled_in_guild(self, ctx.guild):
+            return
+        if not await self.bot.ignored_channel_or_guild(ctx):
+            return
+        if not await self.bot.allowed_by_whitelist_blacklist(ctx.author):
+            return
+
+        command = str(ctx.command)
+
+        if command in self.commands_run.keys():
+            self.commands_run[command] += 1
+            return
+        self.commands_run[command] = 1
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         context = super().format_help_for_context(ctx)
@@ -28,11 +48,6 @@ class AdvancedUptime(commands.Cog):
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete"""
         return
-
-    async def initialize(self) -> None:
-        if 719988449867989142 in self.bot.owner_ids:
-            with contextlib.suppress(Exception):
-                self.bot.add_dev_env_value("advanceduptime", lambda x: self)
 
     def cog_unload(self):
         global _old_uptime
@@ -46,46 +61,68 @@ class AdvancedUptime(commands.Cog):
     @commands.command()
     async def uptime(self, ctx: commands.Context):
         """Shows [botname]'s uptime."""
+
+        def format_timedelta(delta: timedelta, unit: str):
+            mapper = {"seconds": 1, "minutes": 60, "hours": 3600, "days": 86400}
+            return humanize_number(round(delta.total_seconds() / mapper[unit]))
+
         delta = datetime.utcnow() - self.bot.uptime
-        uptime_str = humanize_timedelta(timedelta=delta) or (
-            "Less than one second"
-        )  # Thankyou Red-DiscordBot
-        botname = ctx.bot.user.name
-        users = humanize_number(len(self.bot.users))
-        servers = humanize_number(len(self.bot.guilds))
-        commands_available = humanize_number(len(set(self.bot.walk_commands())))
+        description = f"{self.bot.user} has been up for {bold(humanize_timedelta(timedelta=delta) or 'less than one second')}.\n\n"
+
+        embed = discord.Embed(
+            title="\N{LARGE GREEN CIRCLE} Uptime Information",
+            description=description,
+            color=await ctx.embed_colour(),
+            timestamp=datetime.now(),
+        )
+
+        data = {}
+        units = ("seconds", "minutes", "hours", "days")
+        for unit in units:
+            data[unit] = str(format_timedelta(delta, unit))
+
+        plural_unit = lambda x: f"{data[x]} {x[:-1]}" if data[x] == 1 else f"{data[x]} {x}"
+
+        unit_details = "\n".join(f"+ {plural_unit(x)}" for x in units)
+        embed.add_field(name="Unit Details", value=box(unit_details, lang="diff"), inline=False)
+
         app_info = await self.bot.application_info()
-        owner = app_info.team.name if app_info.team else app_info.owner
-        if await ctx.embed_requested():
-            e = discord.Embed(
-                title=f":green_circle:  {botname}'s Uptime",
-                color=await ctx.embed_colour(),
-                timestamp=ctx.message.created_at,
+        bot_stats = {
+            "users": humanize_number(len(self.bot.users)),
+            "servers": humanize_number(len(self.bot.guilds)),
+            "commands_available": humanize_number(len(set(self.bot.walk_commands()))),
+            "owner": app_info.team.name if app_info.team else app_info.owner,
+        }
+
+        format_key = lambda x: x.replace("_", " ").capitalize()
+        sorted_bot_stats = sorted(bot_stats.items(), key=lambda x: len(x[0]))
+
+        embed.add_field(
+            name="Bot Stats",
+            value=box(
+                "\n".join(f"{format_key(k)}: {v}" for k, v in sorted_bot_stats), lang="yaml"
+            ),
+            inline=False,
+        )
+
+        if self.commands_run:
+            most_run_command = sorted(self.commands_run.items(), key=lambda x: x[1], reverse=True)
+            format_time = lambda x: f"{x} time" if x == 1 else f"{x} times"
+            command_usage = f"The most used command whilst the bot has been online is `{ctx.clean_prefix}{most_run_command[0][0]}`, which has been used {format_time(most_run_command[0][1])}."
+            if len(self.commands_run) != 1:
+                command_usage += f"\n\nThe least used command is `{ctx.clean_prefix}{most_run_command[-1][0]}`, which has been used {format_time(most_run_command[-1][1])}."
+
+            embed.add_field(
+                name="Command usage during this uptime", value=command_usage, inline=False
             )
-            e.add_field(name=f"{botname} has been up for...", value=uptime_str, inline=False)
-            e.add_field(name="Instance owner", value=owner, inline=True)
-            e.add_field(name="Number of guilds", value=servers, inline=False)
-            e.add_field(name="Unique users", value=users, inline=False)
-            e.add_field(name="Commands available", value=commands_available, inline=False)
-            e.set_thumbnail(url=ctx.bot.user.avatar_url)
-            await ctx.send(embed=e)
-        else:
-            title = f"[{botname} has been up for {uptime_str}.]"
-            msg = (
-                f"Instance owner: {owner}\n"
-                f"Number of guilds: {servers}\n"
-                f"Unique users: {users}\n"
-                f"Commands available: {commands_available}"
-            )
-            await ctx.send(box(title, lang="yaml"))
-            await ctx.send(box(msg, lang="yaml"))
+
+        await ctx.send(embed=embed)
 
 
-async def setup(bot):
+def setup(bot):
     au = AdvancedUptime(bot)
     global _old_uptime
     _old_uptime = bot.get_command("uptime")
     if _old_uptime:
         bot.remove_command(_old_uptime.name)
-    await au.initialize()
     bot.add_cog(au)
