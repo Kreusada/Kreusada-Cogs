63b7f1b1b9ae0e8ac84e0bcb766162f3188bca62
Kreusada <kreusadaprojects@gmail.com>
2021-07-18 17:23:14 +0100

    isort

--- raffle/commands/editor/__init__.py
+++ raffle/commands/editor/__init__.py
@@ -1,9 +1,9 @@
-from ...mixins.metaclass import MetaClass
 from .allowed import Allowed
 from .badges import Badges
 from .editor import EditorCommands as Editor
 from .prevented import Prevented
 from .role_requirements import RoleRequirements
+from ...mixins.metaclass import MetaClass
 
 mixins = (Allowed, Badges, Prevented, RoleRequirements, Editor)
 
