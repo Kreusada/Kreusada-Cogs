f157d9f8ab1bb12610ea3935c1eb8f678b0be66f
Kreusada <kreusadaprojects@gmail.com>
2021-07-19 22:15:19 +0100

    Initial commit

--- make.bat
+++ make.bat
@@ -26,6 +26,18 @@ isort --atomic --check --diff --line-length 99 --use-parentheses .
 black --check --diff -l 99 .
 exit /B %ERRORLEVEL%
 
+:gettext
+redgettext --command-docstrings --verbose --recursive --exclude-files "docs/*" .
+exit /B %ERRORLEVEL%
+
+:upload_translations
+crowdin upload sources
+exit /B %ERRORLEVEL%
+
+:download_translations
+crowdin download
+exit /B %ERRORLEVEL%
+
 :help
 echo Usage:
 echo   make ^<command^>
@@ -35,3 +47,6 @@ echo   reformat                   Reformat all .py files being tracked by git.
 echo   isort                      Reformat all .py files only with isort.
 echo   black                      Reformat all .py files only with black.
 echo   stylediff                  Check .py files for style diffs.
+echo   gettext                    Use redgettext
+echo   upload_translations        Upload crowdin translations
+echo   download_translations      Download crowdin translations
