6b8a195aa67c12c28bf3ead4a36484a53319e0e4
Kreusada <67752638+Kreusada@users.noreply.github.com>
2023-01-18 19:55:09 +0000

    Initial

--- vinfo/__init__.py
+++
@@ -1,6 +0,0 @@
-from .vinfo import Vinfo, __red_end_user_data_statement__
-
-
-def setup(bot):
-    cog = Vinfo(bot)
-    bot.add_cog(cog)
--- yamlscanner/__init__.py
+++
@@ -1,5 +0,0 @@
-from .yamlscanner import YamlScanner, __red_end_user_data_statement__
-
-
-def setup(bot):
-    bot.add_cog(YamlScanner(bot))
