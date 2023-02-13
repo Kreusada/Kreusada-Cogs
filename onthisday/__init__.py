6c661b5119ae674bf4425cb08785f8a9a399e2a7
Kreusada <67752638+Kreusada@users.noreply.github.com>
2023-02-13 23:20:25 +0000

    [OnThisDay] Initial changes

--- onthisday/__init__.py
+++ onthisday/__init__.py
@@ -1,27 +1,22 @@
-"""The OnThisDay module. Find out what happened today, in multiple different years in history."""
+"""The OnThisDay module. Find out what happened on a certain day, in multiple different years in history."""
 
 import asyncio
-import contextlib
 import datetime
-import json
-import logging
-import pathlib
-import random
 import re
-
 import aiohttp
 import dateparser
 import discord
+from random import choice
 from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.commands import BadArgument, Context, Converter
-from redbot.core.utils.chat_formatting import humanize_list, inline, warning
+from redbot.core.utils import get_end_user_data_statement
+from redbot.core.utils.chat_formatting import inline, warning
+from typing import Dict, Iterable, Optional, Union
 
-with open(pathlib.Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 ENDPOINT = "https://byabbe.se/on-this-day/{}/events.json"
-log = logging.getLogger("red.kreusada.otd")
 
 DEFAULT_DESCRIPTION = """
 Please send a valid year from the list of years below.
@@ -43,10 +38,6 @@ MONTH_MAPPING = {
     "11": "november",
     "12": "december",
 }
-
-
-def check(ctx: commands.Context):
-    return lambda x: x.author == ctx.author and x.channel == ctx.channel
 
 
 def highlight_numerical_data(string):
@@ -77,6 +68,15 @@ def now() -> datetime.datetime:
     return datetime.datetime.now()
 
 
+def current_year() -> int:
+    return datetime.date.today().year
+
+
+def yield_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
 class DateConverter(Converter):
     """Date converter which uses dateparser.parse()."""
 
@@ -89,128 +89,222 @@ class DateConverter(Converter):
         return parsed
 
 
-class OnThisDay(commands.Cog):
-    """Find out what happened today, in multiple different years in history."""
+class YearDropdown(discord.ui.Select):
+    def __init__(self, otd: "OnThisDay", /):
 
-    __version__ = "1.0.3"
+        self.otd = otd
+        cy = current_year()
+        options = [
+            discord.SelectOption(
+                label=year,
+                description=f"On this day, {cy - int(year)} years ago",
+                emoji="\N{CLOCK FACE THREE OCLOCK}",
+            )
+            for year in self.otd.year_data
+            if int(year) in self.otd.year_range
+        ]
+
+        super().__init__(placeholder="Choose a year...", options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        await self.otd.display_events(
+            interaction,
+            year=self.values[0],
+        )
+
+
+class YearDropdownView(discord.ui.View):
+    def __init__(self, otd: "OnThisDay"):
+        super().__init__()
+        self.add_item(YearDropdown(otd))
+
+
+class YearRangeDropdown(discord.ui.Select):
+    """This exists because the number of years dispatched always
+    exceeds the Select options cap of 25.
+    """
+
+    YEAR_RANGES = [
+        range(1901),
+        range(1901, 1951),
+        range(1951, 2001),
+        range(2001, current_year() + 1),
+    ]
+
+    YEAR_RANGES_HUMANIZED = ["0 - 1900", "1901 - 1950", "1951 - 2000", "2001 - present"]
+
+    def __init__(self, otd: "OnThisDay", /):
+        self.otd = otd
+
+        emojis = [
+            "\N{LARGE RED CIRCLE}",
+            "\N{LARGE ORANGE CIRCLE}",
+            "\N{LARGE YELLOW CIRCLE}",
+            "\N{LARGE GREEN CIRCLE}",
+        ]
+
+        options = [
+            discord.SelectOption(
+                label=self.YEAR_RANGES_HUMANIZED[i],
+                value=i,
+                description=f"Get choice from {self.YEAR_RANGES_HUMANIZED[i].replace('-', 'to')}",
+                emoji=emojis[i],
+            )
+            for i in range(4)
+        ]
+
+        super().__init__(placeholder="Choose a year range...", options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        self.otd.year_range = self.YEAR_RANGES[int(self.values[0])]
+        await interaction.response.edit_message(
+            content=f"Selected year range: **{self.YEAR_RANGES_HUMANIZED[int(self.values[0])]}**\nSelect a year from the list of available years.",
+            view=YearDropdownView(self.otd),
+        )
+
+
+class YearRangeDropdownView(discord.ui.View):
+    def __init__(self, otd: "OnThisDay"):
+        super().__init__()
+        self.add_item(YearRangeDropdown(otd))
+
+
+class OnThisDay(commands.Cog):
+    """Find out what happened on a certain day, in multiple different years in history."""
+
+    __version__ = "2.0.0"
     __author__ = "Kreusada"
 
     def __init__(self, bot: Red):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-        if 719988449867989142 in self.bot.owner_ids:
-            with contextlib.suppress(RuntimeError, ValueError):
-                self.bot.add_dev_env_value(self.__class__.__name__.lower(), lambda x: self)
+        self.date_number: Optional[str] = None
+        self.month_number: Optional[str] = None
+        self.year: Optional[str] = None
+        self.year_data = {}
+        self.year_range: Optional[range] = None
+        self.date_wiki: Optional[str] = None
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         context = super().format_help_for_context(ctx)
         return f"{context}\n\nAuthor: {self.__author__}\nVersion: {self.__version__}"
 
-    def cog_unload(self):
-        self.bot.loop.create_task(self.session.close())
-        if 719988449867989142 in self.bot.owner_ids:
-            with contextlib.suppress(KeyError):
-                self.bot.remove_dev_env_value(self.__class__.__name__.lower())
-
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete"""
         return
 
-    async def run_otd(
-        self, ctx: commands.Context, month_number, date_number, year, _random: bool = False
+    async def cog_unload(self) -> None:
+        await self.session.close()
+
+    async def display_events(
+        self,
+        ctx: Union[commands.Context, discord.Interaction],
+        *,
+        year: str,
     ):
-        """Oh my, is this scruffy code... but it WORKS!"""
+        event = self.year_data[year]
+        years_ago = int(self.year) - int("".join(filter(str.isdigit, year)))
+        embed = discord.Embed(
+            title=f"On this day, {years_ago} years ago...",
+            description=highlight_numerical_data(event["content"]),
+            color=await self.bot.get_embed_colour(ctx.channel),
+        )
+        embed.set_footer(text="Year: " + year + f" | {MONTH_MAPPING[self.month_number]} the {date_suffix(self.date_number)}")
+        embed.add_field(
+            name="Specific Wikipedia Links",
+            value="\n".join(
+                f"- [{w['title']}]({w['wikipedia']})" for w in event["wikipedia"]
+            ),
+        )
+        _d = {
+            f"Other important events that occured in the year {year}...": "https://en.wikipedia.org/wiki/"
+            + year,
+            f"Other important events that occured on {MONTH_MAPPING[self.month_number]} the {date_suffix(self.date_number)} in history...": self.date_wiki,
+        }
+        embed.add_field(
+            name="Other links",
+            value="\n".join(f"- [[{k}]]({v})" for k, v in _d.items()),
+            inline=False,
+        )
+        kwargs = {"embed": embed}
+        if isinstance(ctx, discord.Interaction):
+            send_method = ctx.response.edit_message
+            humanized = YearRangeDropdown.YEAR_RANGES_HUMANIZED[
+                YearRangeDropdown.YEAR_RANGES.index(self.year_range)
+            ]
+            kwargs["content"] = f"Selected year range: **{humanized}**"
+        else:
+            send_method = ctx.send
+        await send_method(**kwargs)
+
+    async def run_otd(
+        self,
+        ctx: commands.Context,
+        date: Optional[datetime.datetime],
+        random: bool = False,
+    ):
+        self.cache_date(date)
         try:
             async with self.session.get(
-                ENDPOINT.format(f"{month_number}/{date_number}")
+                ENDPOINT.format(f"{self.month_number}/{self.date_number}")
             ) as session:
                 if session.status != 200:
                     return await ctx.maybe_send_embed(
-                        warning("An error occured whilst retrieving information for this day.")
+                        warning(
+                            "An error occured whilst retrieving information for this day."
+                        )
                     )
                 content = await session.json()
-        except aiohttp.ClientError as e:
-            log.exception(e)
+        except aiohttp.ClientError:
             return await ctx.maybe_send_embed(
                 warning("An error occured whilst retrieving information for this day.")
             )
         else:
-            wikipedia_for_the_day = content["wikipedia"]
+            self.date_wiki = content["wikipedia"]
             events = filter(lambda x: retrieve_above_0(x["year"]), content["events"])
             data = {
                 e["year"]: {"content": e["description"], "wikipedia": e["wikipedia"]}
                 for e in events
             }
-            if _random:
-                result = random.choice(list(data.keys()))
+            self.year_data = data
+            if random:
+                await self.display_events(ctx, year=choice(list(data.keys())))
             else:
-                message = f"**Events for this day ({MONTH_MAPPING[month_number].capitalize()} the {date_suffix(date_number)})**\n"
-                await ctx.maybe_send_embed(
-                    message + DEFAULT_DESCRIPTION + columns(list(data.keys()))
+                await ctx.send(
+                    "Choose a year range to select from:",
+                    view=YearRangeDropdownView(self),
                 )
-                try:
-                    message = await self.bot.wait_for("message", check=check(ctx), timeout=50)
-                except asyncio.TimeoutError:
-                    return await ctx.send("You took too long to respond.")
-                container = tuple(data.keys())
-                if (result := message.content.lstrip("0")) not in container:
-                    return await ctx.send(f"{inline(result)} was not a valid year for this day.")
-            event = data[result]
-            years_ago = int(year) - int("".join(filter(str.isdigit, result)))
-            embed = discord.Embed(
-                title=f"On this day, {years_ago} years ago...",
-                description=highlight_numerical_data(event["content"]),
-                color=await ctx.embed_colour(),
-            )
-            embed.set_footer(text="Year: " + result)
-            embed.add_field(
-                name="Specific Wikipedia Links",
-                value="\n".join(f"- [{w['title']}]({w['wikipedia']})" for w in event["wikipedia"]),
-            )
-            _d = {
-                f"Other important events that occured in the year {result}...": "https://en.wikipedia.org/wiki/"
-                + result,
-                f"Other important events that occured on {MONTH_MAPPING[month_number]} the {date_suffix(date_number)} in history...": wikipedia_for_the_day,
-            }
-            embed.add_field(
-                name="Other links",
-                value="\n".join(f"- [[{k}]]({v})" for k, v in _d.items()),
-                inline=False,
-            )
-            await ctx.send(embed=embed)
+
+    def cache_date(self, date: Optional[datetime.datetime], /) -> Dict[str, int]:
+        if date is None:
+            date = now()
+
+        self.month_number = date.strftime(r"%m").lstrip("0")
+        self.date_number = date.strftime(r"%d").lstrip("0")
+        self.year = date.strftime(r"%Y")
 
     @commands.has_permissions(embed_links=True)
     @commands.group(invoke_without_command=True, aliases=["otd"])
-    async def onthisday(self, ctx, *, day: DateConverter = None):
+    async def onthisday(self, ctx: commands.Context, *, date: DateConverter = None):
         """Find out what happened on this day, in various different years!
 
         If you want to specify your own date, you can do so by using
-        `[p]onthisday [day]`.
+        `[p]onthisday [date]`.
         You can also receive a random year by using `[p]onthisday random [day]`.
         """
-        await ctx.trigger_typing()
-        if day is None:
-            day = now()
-        month_number = day.strftime(r"%m").lstrip("0")
-        date_number = day.strftime(r"%d").lstrip("0")
-        year = day.strftime(r"%Y")
-        await self.run_otd(ctx, month_number, date_number, year, False)
+        await ctx.typing()
+        await self.run_otd(ctx, date=date, random=False)
 
     @onthisday.command()
-    async def random(self, ctx, *, day: DateConverter = None):
+    async def random(self, ctx: commands.Context, *, date: DateConverter = None):
         """Find out what happened on this day, in a random year.
 
         If you want to specify your own date, you can do so by using
-        `[p]onthisday random [day]`.
+        `[p]onthisday [date]`.
         """
-        await ctx.trigger_typing()
-        if day is None:
-            day = now()
-        month_number = day.strftime(r"%m").lstrip("0")
-        date_number = day.strftime(r"%d").lstrip("0")
-        year = day.strftime(r"%Y")
-        await self.run_otd(ctx, month_number, date_number, year, True)
+        await ctx.typing()
+        await self.run_otd(ctx, date=date, random=True)
 
 
-def setup(bot):
-    bot.add_cog(OnThisDay(bot))
+async def setup(bot: Red):
+    await bot.add_cog(OnThisDay(bot))
