03ee92d04624355172a001b2dda2158ac30de83c
Kreusada <67752638+Kreusada@users.noreply.github.com>
2023-05-01 18:41:58 +0100

    [Termino] Appropriate DPY2 changes

--- termino/__init__.py
+++ termino/__init__.py
@@ -1,1 +1,6 @@
-from .termino import __red_end_user_data_statement__, setup
+from .termino import setup
+
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
