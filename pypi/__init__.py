db62e7ccaa4537cd1c6ef41b7b82887889cead46
Kreusada <67752638+Kreusada@users.noreply.github.com>
2023-05-01 17:11:18 +0100

    [PyPi] Improvements to `__init__.py`

--- pypi/__init__.py
+++ pypi/__init__.py
@@ -1,8 +1,10 @@
-from redbot.core.utils import get_end_user_data_statement
 from .pypi import PyPi
+
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
-async def setup(bot):
-    cog = PyPi(bot)
-    await bot.add_cog(cog)
+
+async def setup(bot: Red):
+    await bot.add_cog(PyPi(bot))
